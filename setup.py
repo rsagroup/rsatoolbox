6119588b1c15eef9b1ff83b3d60531646719ea7b
Jasper van den Bosch <japsai@gmail.com>
2019-10-07 09:25:48 -0400

    added automatic long description from README

--- setup.py
+++ setup.py
@@ -5,12 +5,17 @@ with open('requirements.txt') as reqfile:
     requires = reqfile.read().splitlines()
 
 
+with open('README.md', encoding='utf-8') as readmefile:
+    long_description = readmefile.read()
+
+
 setup(
     name='pyrsa',
     version='3.0.0',
     description='Python Representational Similarity Analysis (RSA)',
     url='https://github.com/rsagroup/pyrsa',
-    long_description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers=[
       "Programming Language :: Python",
       "Development Status :: 1 - Planning",
