4d00362b8a59798bc078c3e4a654849bee896674
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2019-10-08 16:45:53 -0400

    Initial model definition for Model + Model Fixed

---
+++ pyrsa/model/model.py
@@ -0,0 +1,45 @@
+"""
+Definition of RSA Model class and subclasses 
+@author: jdiedrichsen,
+"""
+
+import numpy as np 
+
+
+"""
+Abstract model class. 
+Defines members that every class needs to have, but does not implement any interesting 
+behavior. Inherit from this class to define specific model types 
+"""
+class Model:
+    # Model Constructor 
+    def __init__(self,name):
+        self.name = name
+        self.n_param = 0
+        
+    # The predict function should return a vector of parameters     
+    def predict(self,theta):
+        raise(NameError("This function needs to be implemented in the derived class"))
+        
+    
+"""
+Fixed model
+This is a parameter-free model that simply predicts a fixed RDM 
+It takes a unidimension numpy-vector as an input to define the RDM
+"""    
+class ModelFixed(Model):
+    # Model Constructor 
+    def __init__(self,name,rdm): 
+        Model.__init__(self,name)
+        if (rdm.ndim == 1):  # User passed a vector 
+            self.n_cond = (1+np.sqrt(1+8*rdm.size))/2 
+            if (self.n_cond%1!=0):
+                raise (NameError("RDM vector needs to have size of ncond*(ncond-1)/2"))
+            self.rdm = rdm   # Add check to make sure it's 
+            self.n_param = 0
+    
+    # prediction returns the 
+    def predict(self,theta):
+        return self.rdm
+
+        
