3b70d731715d7bee21ff1d0397675123885082f5
Baihan Lin <nature@gatech.edu>
2019-10-19 02:26:34 -0400

    [DBG] style fix on neurodata

--- pyrsa/data/neurodataset.py
+++ pyrsa/data/neurodataset.py
@@ -8,24 +8,28 @@ import pyrsa as rsa
 from pyrsa.data.dataset import DatasetBase
 import numpy as np
 
+
 def preprocess_fmri(rawdata=None):
-    raise NotImplementedError(
-            "preprocess_fmri function not implemented!")
+    raise NotImplementedError( 
+        "preprocess_fmri function not implemented!")
 
-class DatasetFmri(DatasetBase): 
+class DatasetFmri(DatasetBase):
     """
-    DatasetFmri class is a variant of Dataset that takes in raw fMRI data and process it into standard Dataset format.
+    DatasetFmri class is a variant of Dataset that takes in raw 
+    fMRI data and process it into standard Dataset format.
     """
-    def __init__(self, rawdata=None,preprocess=preprocess_fmri):
-        measurements,descriptors,obs_descriptors,channel_descriptors=preprocess(rawdata)
+    def __init__(self, rawdata=None, preprocess=preprocess_fmri):
 
-        if (measurements.ndim==2):
+        measurements, descriptors, obs_descriptors, channel_descriptors \
+            = preprocess(rawdata)
+
+        if (measurements.ndim == 2):
             self.measurements = measurements
-            self.n_set = 1 
-            self.n_obs,self.n_channel = self.measurements.shape
-        elif (measurements.ndim==3):
+            self.n_set = 1
+            self.n_obs, self.n_channel = self.measurements.shape
+        elif (measurements.ndim == 3):
             self.measurements = measurements
-            self.n_set,self.n_obs,self.n_channel = self.measurements.shape
-        self.descriptors = descriptors 
-        self.obs_descriptors = obs_descriptors 
-        self.channel_descriptors = channel_descriptors 
+            self.n_set, self.n_obs, self.n_channel = self.measurements.shape
+        self.descriptors = descriptors
+        self.obs_descriptors = obs_descriptors
+        self.channel_descriptors = channel_descriptors
