249c2101425e91947636c73acf2d64b0c807cf6b
Baihan Lin <nature@gatech.edu>
2019-10-19 02:34:34 -0400

    [DBG] style fix on data-dev

--- pyrsa/data/exampledataset.py
+++ pyrsa/data/exampledataset.py
@@ -4,21 +4,30 @@
 Definition of RSA Dataset classes for arbitrary user-defined data
 @author: baihan
 """
+import numpy as np
 import pyrsa as rsa
 from pyrsa.data.dataset import DatasetBase
-import numpy as np
 
-def preprocess_example(rawdata=None):
-    raise NotImplementedError(
-            "preprocess_fmri function not implemented!")
 
-class DatasetExample(DatasetBase): 
+def preprocess(rawdata=None):
+    """example of a preprocessing function
+        Args:
+            rawdata (pyrsa.data.dataset): the dataset
+
+        Returns:
+            preprocessed dataset
     """
-    DatasetExample class is a variant of Dataset that takes in any arbitrary user defined data and process it into standard Dataset format.
+    raise NotImplementedError(
+        "preprocess_fmri function not implemented!")
+
+class DatasetExample(DatasetBase):
+    """
+    DatasetExample class is a variant of Dataset that takes in any arbitrary 
+    user defined data and process it into standard Dataset format.
     It also enables the user to specifies arbitrary subset or split methods.
     """
-    def __init__(self, rawdata=None,preprocess=preprocess_example):
-        measurements,descriptors,obs_descriptors,channel_descriptors=preprocess(rawdata)
+    def __init__(self, rawdata=None, preprocess=preprocess_example):
+        measurements, descriptors, obs_descriptors, channel_descriptors = preprocess(rawdata)
 
         if (measurements.ndim==2):
             self.measurements = measurements
@@ -40,7 +49,6 @@ class DatasetExample(DatasetBase):
             list of Datasets, splitted by the selected obs_descriptor
         """
         # TODO
-        
 
     def split_channel(self, by):
         """ Returns a list Datasets splited by channels
@@ -55,8 +63,10 @@ class DatasetExample(DatasetBase):
     def subset_obs(self, by, value):
         """ Returns a subsetted Dataset defined by certain obs value
         Args:
-            by(String): the descriptor by which the subset selection is made from obs dimension
-            value: the value by which the subset selection is made from obs dimension
+            by(String): the descriptor by which the subset selection is 
+                        made from obs dimension
+            value:      the value by which the subset selection is made 
+                        from obs dimension
 
         Returns:
             Dataset, with subset defined by the selected obs_descriptor
@@ -66,8 +76,10 @@ class DatasetExample(DatasetBase):
     def subset_channel(self, by, value):
         """ Returns a subsetted Dataset defined by certain channel value
         Args:
-            by(String): the descriptor by which the subset selection is made from channel dimension
-            value: the value by which the subset selection is made from channel dimension
+            by(String): the descriptor by which the subset selection is 
+                        made from channel dimension
+            value:      the value by which the subset selection is made 
+                        from channel dimension
 
         Returns:
             Dataset, with subset defined by the selected channel_descriptor
