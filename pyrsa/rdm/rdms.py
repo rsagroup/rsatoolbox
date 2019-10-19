a39e3ee853d4fe5a4cd5d0f745b3f566a37f99a5
Baihan Lin <nature@gatech.edu>
2019-10-19 03:22:37 -0400

    [DBG] style fix in rdm

--- pyrsa/rdm/rdms.py
+++ pyrsa/rdm/rdms.py
@@ -7,7 +7,7 @@ Definition of RSA RDMs class and subclasses
 
 import numpy as np
 from scipy.spatial.distance import squareform
-# import pyrsa as rsa
+
 
 class RDMs:
     """
@@ -17,23 +17,26 @@ class RDMs:
             dissimilarities (numpy.ndarray):
                 either a 2d np-array (n_rdm x vectorform of dissimilarities)
                 or a 3d np-array (n_rdm x n_cond x n_cond)
-            dissimilarity_measure (String):     a description of the dissimilarity measure
-                (e.g. 'Euclidean')      
-            descriptors (dict):     descriptors with 1 value per RDMs object
+            dissimilarity_measure (String):
+                a description of the dissimilarity measure (e.g. 'Euclidean')
+            descriptors (dict):
+                descriptors with 1 value per RDMs object
         Returns:
             RDMs object
     """
-    def __init__(self, dissimilarities=None, dissimilarity_measure=None, descriptors=None):
+    def __init__(self, dissimilarities=None,
+                 dissimilarity_measure=None,
+                 descriptors=None):
         if (dissimilarities.ndim == 2):
             self.dissimilarities = dissimilarities
             self.n_rdm = self.measurements.shape[0]
-            self.n_cond = np.ceil(np.sqrt(self.measurements.shape[0]*2))
+            self.n_cond = np.ceil(np.sqrt(self.measurements.shape[0] * 2))
         elif (dissimilarities.ndim == 3):
             self.dissimilarities = dissimilarities
             self.n_rdm = self.dissimilarities.shape[0]
             self.n_cond = self.dissimilarities.shape[1]
-        self.descriptors = descriptors 
-        self.dissimilarity_measure = dissimilarity_measure 
+        self.descriptors = descriptors
+        self.dissimilarity_measure = dissimilarity_measure
 
     def get_vectors(self):
         """ Returns RDMs as np.ndarray with each RDM as a vector
