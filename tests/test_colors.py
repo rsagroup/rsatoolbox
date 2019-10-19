c6a9d97b6eed0b69739e1ad86dbae4e5d61ac9b7
Ian Charest <i.charest@bham.ac.uk>
2019-10-19 20:38:57 +0100

    added colors.py [color_scale, rdm_colormap] and test_colors.py [passed]

---
+++ tests/test_colors.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+test_colors
+Test for colors 
+@author: iancharest
+"""
+
+import unittest
+from pyrsa.vis.colors import color_scale
+from pyrsa.vis.colors import rdm_colormap
+
+class ColorTests(unittest.TestCase):
+
+    def test_color_scale(self):
+        n_cols = 10
+        cols = color_scale(n_cols)
+        n_cols_returned, n_rgb = cols.shape
+        self.assertEqual(n_cols_returned, n_cols)
+        self.assertEqual(n_rgb, 3)
+
+    def test_rdm_colormap(self):
+        n_cols = 10
+        cols = rdm_colormap(n_cols)
+        n_cols_returned, n_rgb = cols.colors.shape
+        self.assertEqual(n_cols_returned, n_cols)
+        self.assertEqual(n_rgb, 3)
+
+if __name__ == '__main__':
+    unittest.main()     
