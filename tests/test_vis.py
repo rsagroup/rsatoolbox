233e494055246df1dc9d85881d49f2794189ad03
Baihan Lin <nature@gatech.edu>
2019-11-02 13:45:16 -0400

    [FEAT] initialize vis testing

---
+++ tests/test_vis.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+test_data
+Test for visualization class 
+@author: baihan
+"""
+
+import unittest 
+import pyrsa.vis as vis
+import numpy as np 
+
+
+class TestVis(unittest.TestCase): 
+    
+    def test_MDS(self):
+        # more to come
+
+if __name__ == '__main__':
+    unittest.main()  
