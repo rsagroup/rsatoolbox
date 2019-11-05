9d5719daf46974ed7a3845c051a446bf0bc47ce9
Heiko Schuett <heiko.schuett@nyu.edu>
2019-11-05 15:48:13 -0500

    added imports of submodules into main module

--- pyrsa/__init__.py
+++ pyrsa/__init__.py
@@ -2,3 +2,11 @@
 # -*- coding: utf-8 -*-
 """ Top level package: Only imports and organisation
 """
+import pyrsa.data as data
+import pyrsa.inference as inference
+import pyrsa.model as model
+import pyrsa.rdm as rdm
+import pyrsa.simulation as simulation
+import pyrsa.util as util
+import pyrsa.vis as vis
+
