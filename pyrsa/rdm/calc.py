dec7a7ff0866100910da52c9f3ee7362d3e7f386
Ian Charest <i.charest@bham.ac.uk>
2019-11-25 22:20:24 +0000

    corr dist using einsum and test case using np.corrcoef

--- pyrsa/rdm/calc.py
+++ pyrsa/rdm/calc.py
@@ -84,10 +84,14 @@ def calc_rdm_correlation(dataset, descriptor=None):
         Returns:
             RDMs object with the one RDM
     """
-    measurements, desc, descriptor = _parse_input(dataset, descriptor)
-    rdm = 1 - np.corrcoef(measurements)
+    ma, desc, descriptor = _parse_input(dataset, descriptor)
+
+    ma = ma - ma.mean(axis=1, keepdims=True)
+    ma /= np.sqrt(np.einsum('ij,ij->i', ma, ma))[:, None]
+    rdm = 1 - np.einsum('ik,jk', ma, ma)
+   
     rdm = RDMs(dissimilarities=np.array([rdm]),
-               dissimilarity_measure='euclidean',
+               dissimilarity_measure='correlation',
                descriptors=dataset.descriptors)
     rdm.pattern_descriptors[descriptor] = desc
     return rdm
