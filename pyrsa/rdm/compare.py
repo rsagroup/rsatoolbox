8ea7c24fec4194be55c77210cc1d058a43b92ae2
Heiko Schuett <heiko.schuett@nyu.edu>
2019-12-06 15:57:01 -0500

    style update

--- pyrsa/rdm/compare.py
+++ pyrsa/rdm/compare.py
@@ -7,6 +7,7 @@ Created on Fri Dec  6 13:01:12 2019
 """
 import numpy as np
 
+
 def compare_cosine(rdm1, rdm2):
     """
     calculates the cosine distance between two RDMs objects
@@ -24,9 +25,9 @@ def compare_cosine(rdm1, rdm2):
     vector2 = rdm2.get_vectors()
     if not (vector1.shape[1] == vector2.shape[1]):
         raise ValueError('rdm1 and rdm2 must be RDMs of equal shape')
-    vector1 = np.mean(vector1,0)
-    vector2 = np.mean(vector2,0)
+    vector1 = np.mean(vector1, 0)
+    vector2 = np.mean(vector2, 0)
     dist = 1 - (np.sum(vector1*vector2) /
-                  np.sqrt(np.sum(vector1*vector1)) /
-                  np.sqrt(np.sum(vector2*vector2)))
+                np.sqrt(np.sum(vector1 * vector1)) /
+                np.sqrt(np.sum(vector2 * vector2)))
     return dist
