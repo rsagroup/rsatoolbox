cf440026f7d632656bba93e38620ac2d8d7d97a8
Heiko Schuett <heiko.schuett@nyu.edu>
2020-01-13 15:20:15 -0500

    fixed model evaluation

---
+++ pyrsa/inference/evaluate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+inference module: evaluate models
+@author: heiko
+"""
+
+import numpy as np
+from pyrsa.rdm import compare
+
+
+def eval_fixed(model, data, method='cosine'):
+    """evaluates a model on data, without any bootstrapping or 
+    cross-validation
+
+    Args:
+        model(pyrsa.model.Model): Model to be evaluated
+
+        data(pyrsa.rdm.RDMs): data to evaluate on
+
+    Returns:
+        float: evaluation
+
+    """
+    rdm_pred = model.predict()
+    return compare(rdm_pred, data, method) 
