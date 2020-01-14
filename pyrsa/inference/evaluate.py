88a4ba70d9069f043ac27418da8e638e1451554f
Heiko Schuett <heiko.schuett@nyu.edu>
2020-01-14 12:11:06 -0500

    skaffold for eval_bootstrap_rdm

--- pyrsa/inference/evaluate.py
+++ pyrsa/inference/evaluate.py
@@ -18,9 +18,33 @@ def eval_fixed(model, data, method='cosine'):
 
         data(pyrsa.rdm.RDMs): data to evaluate on
 
+        method(string): comparison method to use
+
     Returns:
         float: evaluation
 
     """
     rdm_pred = model.predict()
     return compare(rdm_pred, data, method)
+
+
+def eval_bootstrap_rdm(model, data, method, N=1000,
+                       rdm_descriptor=None):
+    """evaluates a model on data
+    performs 
+
+    Args:
+        model(pyrsa.model.Model): Model to be evaluated
+
+        data(pyrsa.rdm.RDMs): data to evaluate on
+
+        method(string): comparison method to use
+
+        N(int): number of samples
+
+        rdm_descriptor(string): rdm_descriptor to group rdms for bootstrap
+
+    Returns:
+        float: evaluation
+
+    """
