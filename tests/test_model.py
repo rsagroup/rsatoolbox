7194f0f45e0802ac41ec574d4588be198c1ae644
Heiko Schuett <heiko.schuett@nyu.edu>
2020-01-15 10:55:24 -0500

    test RDM based model creation

--- tests/test_model.py
+++ tests/test_model.py
@@ -19,8 +19,18 @@ class test_ModelFixed(unittest.TestCase):
     """
     def test_creation(self):
         rdm = np.array(np.ones(6))
-        m = model.ModelFixed('Test Model',rdm)
+        m = model.ModelFixed('Test Model', rdm)
         m.fit([])
         pred = m.predict()
         assert np.all(pred == rdm)
-        
+
+    def test_creation_rdm(self):
+        from pyrsa.rdm import RDMs
+        rdm = np.array(np.ones(6))
+        rdm_obj = RDMs(np.array([rdm]))
+        m = model.ModelFixed('Test Model', rdm_obj)
+        m.fit(rdm_obj)
+        pred = m.predict()
+        assert np.all(pred == rdm)
+        pred_obj = m.predict_rdm()
+        assert isinstance(pred_obj, RDMs)
