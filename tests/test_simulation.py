236ff78e63dead4e2bb794ea3614b2c0c9469e8d
Heiko Schütt <heiko.schuett@nyu.edu>
2020-01-20 11:10:47 -0500

    Update test_simulation.py

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -4,23 +4,26 @@
 """
 
 import unittest
-import pyrsa.simulation.sim as sim
 import pyrsa.model as model
 import numpy as np
 
+
 class TestSimulation(unittest.TestCase):
     def test_make_design(self):
+        import pyrsa.simulation.sim as sim
         # Test for make_design 
         cond_vec,part_vec = sim.make_design(4, 8)
         self.assertEqual(cond_vec.size,32)
 
     def test_make_data(self):
         # Test for make_data 
+        import pyrsa.simulation.sim as sim
         cond_vec,part_vec = sim.make_design(4, 8)
         M = model.ModelFixed("test", np.array([2, 2, 2, 1, 1, 1]))
         D = sim.make_dataset(M, None, cond_vec, n_channel=40)
         self.assertEqual(D[0].n_obs, 32)
         self.assertEqual(D[0].n_channel, 40)
+
 
 if __name__ == '__main__':
     unittest.main()
