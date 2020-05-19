b22bee871774e6aa14427352e926ebf5f98c81c1
Heiko Schuett <heiko.schuett@nyu.edu>
2020-05-19 16:35:05 +0200

    cluster script eco-sim

---
+++ stats/cluster_eco_sim.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=8:00:00
+#SBATCH --mem=8GB
+#SBATCH --job-name=eco_sim
+#SBATCH --mail-type=END
+#SBATCH --mail-user=hhs4@nyu.edu
+#SBATCH --output=slurm-output/slurm_%a.out
+
+index=$SLURM_ARRAY_TASK_ID
+job=$SLURM_JOB_ID
+ppn=$SLURM_JOB_CPUS_PER_NODE
+
+module purge
+module load anaconda3/5.3.1
+
+source activate RSA
+
+which python
+
+~/.conda/envs/RSA/bin/python stats.py -p /scratch/hhs4/ecoset/val eco_sim $index
