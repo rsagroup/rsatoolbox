8e5a2d93888492cf3e632e50c135c0959fae16b1
Heiko Schuett <heiko.schuett@nyu.edu>
2020-05-20 17:53:54 +0200

    correction

--- stats/cluster_eco.sh
+++ stats/cluster_eco.sh
@@ -7,7 +7,7 @@
 #SBATCH --job-name=eco_full
 #SBATCH --mail-type=END
 #SBATCH --mail-user=hhs4@nyu.edu
-#SBATCH --output=slurm-output/slurm_ana_%a.out
+#SBATCH --output=slurm-output/slurm_eco_%a.out
 
 index=$SLURM_ARRAY_TASK_ID
 job=$SLURM_JOB_ID
