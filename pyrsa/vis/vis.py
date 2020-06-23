186592ebe131447788e04bfbdb08c3af1b7531d4
Baihan Lin <nature@gatech.edu>
2020-06-23 14:01:41 -0400

    [FEAT] add mds and test for 1 rdm

--- pyrsa/vis/vis.py
+++ pyrsa/vis/vis.py
@@ -9,3 +9,37 @@ import pyrsa as rsa
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.manifold import MDS
+from sklearn.decomposition import PCA
+
+sd = np.random.RandomState(seed=1)
+mds = MDS(n_components=2, random_state=sd, dissimilarity="precomputed")
+
+def dimension_reduction(rdms, func):
+    """ dimension reduction of RDMs class
+
+    Args:
+        rdms (RDMs class): an RDMs class object
+        func (function): an sklearn transform function
+
+    Returns:
+        dr (numpy.ndarray): a dimension-reduced 
+        embedding
+
+    """
+    rdmm = rdms.get_matrices()
+    dr = func.fit_transform(rdmm)
+    return dr
+
+def mds(rdms):
+    """ multi-dimensional scaling of RDMs class
+
+    Args:
+        rdms (RDMs class): an RDMs class object
+
+    Returns:
+        (numpy.ndarray): an MDS embedding
+
+    """
+    return dimension_reduction(rdms,mds)
+
--- tests/test_vis.py
+++ tests/test_vis.py
@@ -7,9 +7,21 @@ Test for visualization class
 """
 
 import unittest 
-import pyrsa.vis as vis
+import pyrsa.vis as rsv
+import pyrsa.rdm as rsr
 import numpy as np 
 
+class TestVIS(unittest.TestCase): 
+    
+    def test_rdm2d_mds(self):
+        dis = np.zeros((8,10))
+        mes = "Euclidean"
+        des = {'session':0, 'subj':0}
+        rdms = rsr.RDMs(dissimilarities=dis,
+                        dissimilarity_measure=mes,
+                        descriptors=des)
+        mds_emb = rsv.mds(rdms)
+        self.assertEqual(mds_emb.shape,(5, 2))
 
 if __name__ == '__main__':
     unittest.main()  
