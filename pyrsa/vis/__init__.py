2b7f15551c557129cc5da0fd6b7501325d5c2974
Baihan Lin <nature@gatech.edu>
2020-06-23 14:12:51 -0400

    [FEAT] add init for vis

--- pyrsa/vis/__init__.py
+++ pyrsa/vis/__init__.py
@@ -1,1 +1,2 @@
+from .vis import mds,dr
 from .model_plot import plot_model_comparison
--- tests/test_vis.py
+++ tests/test_vis.py
@@ -7,7 +7,7 @@ Test for visualization class
 """
 
 import unittest 
-import pyrsa.vis as rsv
+from pyrsa.vis import mds
 import pyrsa.rdm as rsr
 import numpy as np 
 
@@ -20,7 +20,7 @@ class TestVIS(unittest.TestCase):
         rdms = rsr.RDMs(dissimilarities=dis,
                         dissimilarity_measure=mes,
                         descriptors=des)
-        mds_emb = rsv.mds(rdms)
+        mds_emb = mds(rdms)
         self.assertEqual(mds_emb.shape,(8,5,2))
 
 if __name__ == '__main__':
