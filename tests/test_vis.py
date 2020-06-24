4749ffba5e39b6e4d93b35180f75cedcbad9edd0
Jasper van den Bosch <japsai@gmail.com>
2020-06-24 19:21:26 +0200

    pep8 fixes in mds unit test module

--- tests/test_vis.py
+++ tests/test_vis.py
@@ -2,26 +2,28 @@
 # -*- coding: utf-8 -*-
 """
 test_data
-Test for visualization class 
+Test for visualization class
 @author: baihan
 """
-
-import unittest 
+import unittest
+import numpy as np
 import pyrsa.vis as rsv
 import pyrsa.rdm as rsr
-import numpy as np 
 
-class TestVIS(unittest.TestCase): 
-    
+
+
+class TestVIS(unittest.TestCase):
+
     def test_vis_mds_output_shape_corresponds_to_inputs(self):
-        dis = np.zeros((8,10))
+        dis = np.zeros((8, 10))
         mes = "Euclidean"
         des = {'session':0, 'subj':0}
         rdms = rsr.RDMs(dissimilarities=dis,
                         dissimilarity_measure=mes,
                         descriptors=des)
         mds_emb = rsv.vis.mds(rdms)
-        self.assertEqual(mds_emb.shape,(8,5,2))
+        self.assertEqual(mds_emb.shape, (8, 5, 2))
+
 
 if __name__ == '__main__':
-    unittest.main()  
+    unittest.main()
