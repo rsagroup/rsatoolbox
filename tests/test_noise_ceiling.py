486e91b53408427ad808bb5abcf2fe750d5257b4
Jasper van den Bosch <japsai@gmail.com>
2020-06-24 20:08:39 +0200

    pep8 fixes in noise ceiling test suite

--- tests/test_noise_ceiling.py
+++ tests/test_noise_ceiling.py
@@ -12,24 +12,25 @@ from parameterized import parameterized
 
 
 class TestNoiseCeiling(unittest.TestCase):
+
     def test_cv_noise_ceiling(self):
         from pyrsa.inference import cv_noise_ceiling
         from pyrsa.inference import sets_k_fold_rdm
         from pyrsa.rdm import RDMs
-        dis = np.random.rand(11,10)  # 11 5x5 rdms
+        dis = np.random.rand(11, 10)  # 11 5x5 rdms
         mes = "Euclidean"
-        des = {'subj':0}
-        rdm_des = {'session':np.array([1,1,2,2,4,5,6,7,7,7,7])}
-        pattern_des = {'type':np.array([0,1,2,2,4])}
-        rdms = RDMs(dissimilarities=dis,
-                    rdm_descriptors=rdm_des,
-                    pattern_descriptors=pattern_des,
-                    dissimilarity_measure=mes,
-                    descriptors=des)
-        train_set, test_set, ceil_set = sets_k_fold_rdm(rdms, k_rdm=3,
-                                                          random=False)
-        noise_min, noise_max = cv_noise_ceiling(rdms, ceil_set, test_set,
-                                                method='cosine')
+        des = {'subj': 0}
+        rdm_des = {'session':np.array([1, 1, 2, 2, 4, 5, 6, 7, 7, 7, 7])}
+        pattern_des = {'type':np.array([0, 1, 2, 2, 4])}
+        rdms = RDMs(
+            dissimilarities=dis,
+            rdm_descriptors=rdm_des,
+            pattern_descriptors=pattern_des,
+            dissimilarity_measure=mes,
+            descriptors=des
+        )
+        _, test_set, ceil_set = sets_k_fold_rdm(rdms, k_rdm=3, random=False)
+        _, _ = cv_noise_ceiling(rdms, ceil_set, test_set, method='cosine')
 
     @parameterized.expand([
         ['cosine'],
@@ -41,14 +42,16 @@ class TestNoiseCeiling(unittest.TestCase):
     def test_boot_noise_ceiling_runs_for_method(self, method):
         from pyrsa.inference import boot_noise_ceiling
         from pyrsa.rdm import RDMs
-        dis = np.random.rand(11,10)  # 11 5x5 rdms
+        dis = np.random.rand(11, 10)  # 11 5x5 rdms
         mes = "Euclidean"
-        des = {'subj':0}
-        rdm_des = {'session':np.array([1,1,2,2,4,5,6,7,7,7,7])}
-        pattern_des = {'type':np.array([0,1,2,2,4])}
-        rdms = RDMs(dissimilarities=dis,
-                    rdm_descriptors=rdm_des,
-                    pattern_descriptors=pattern_des,
-                    dissimilarity_measure=mes,
-                    descriptors=des)
-        noise_min, noise_max = boot_noise_ceiling(rdms, method=method)
+        des = {'subj': 0}
+        rdm_des = {'session': np.array([1, 1, 2, 2, 4, 5, 6, 7, 7, 7, 7])}
+        pattern_des = {'type': np.array([0, 1, 2, 2, 4])}
+        rdms = RDMs(
+            dissimilarities=dis,
+            rdm_descriptors=rdm_des,
+            pattern_descriptors=pattern_des,
+            dissimilarity_measure=mes,
+            descriptors=des
+        )
+        _, _ = boot_noise_ceiling(rdms, method=method)
