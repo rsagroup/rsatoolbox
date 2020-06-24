e126eccf75871c6641e6a76ea555c9361cd01313
Jasper van den Bosch <japsai@gmail.com>
2020-06-24 19:24:24 +0200

    pep8 fixes in vis module

--- pyrsa/vis/vis.py
+++ pyrsa/vis/vis.py
@@ -5,14 +5,12 @@ Definition of visualization methods
 @author: baihan
 """
 
-import pyrsa as rsa
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.manifold import MDS
-from sklearn.decomposition import PCA
 
 sd = np.random.RandomState(seed=1)
 mds_emb = MDS(n_components=2, random_state=sd, dissimilarity="precomputed")
+
 
 def rdm_dimension_reduction(rdms, func):
     """ dimension reduction of RDMs class
@@ -22,15 +20,16 @@ def rdm_dimension_reduction(rdms, func):
         func (function): an sklearn transform function
 
     Returns:
-        dr (numpy.ndarray): a dimension-reduced 
+        dr (numpy.ndarray): a dimension-reduced
         embedding of size (n_rdm x n_cond x n_emb)
 
     """
     rdmm = rdms.get_matrices()
-    drs = np.ndarray((rdms.n_rdm,rdms.n_cond,2))
+    drs = np.ndarray((rdms.n_rdm, rdms.n_cond, 2))
     for i in np.arange(rdms.n_rdm):
-        drs[i,:,:] = func.fit_transform(rdmm[i,:,:])
+        drs[i, :, :] = func.fit_transform(rdmm[i, :, :])
     return drs
+
 
 def mds(rdms):
     """ multi-dimensional scaling of RDMs class
@@ -42,5 +41,4 @@ def mds(rdms):
         (numpy.ndarray): an MDS embedding
 
     """
-    return rdm_dimension_reduction(rdms,mds_emb)
-
+    return rdm_dimension_reduction(rdms, mds_emb)
