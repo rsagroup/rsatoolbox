a342debe8e3d91e5dbf31bca09a41b2c5f380967
Jasper van den Bosch <japsai@gmail.com>
2020-07-21 23:16:48 +0200

    extracting meadows file name info

--- pyrsa/io/meadows.py
+++ pyrsa/io/meadows.py
@@ -1,3 +1,4 @@
+from os.path import basename
 from scipy.io import loadmat
 from pyrsa.rdm.rdms import RDMs
 
@@ -26,4 +27,42 @@ def load_rdms(fpath):
         dissimilarity_measure='euclidean',
         descriptors=dict(),
         pattern_descriptors=dict(),
+    )
+
+
+def extract_filename_segments(fpath):
+    """Get information from the name of a donwloaded results file
+
+    Will determine:
+    - participant_scope: 'single' or 'multiple', how many participant sessions
+        this file covers
+    - task_scope: 'single' or 'multiple', how many experiment tasks this file
+        covers
+    - participant: the Meadows nickname of the participant, if this is a 
+        single participation file.
+    - task_index: the 1-based index of the task in the experiment, if 
+        this is a single task file.
+    - version: the experiment version as a string.
+    - experiment_name: name of the experiment on Meadows.
+    - structure: the structure of the data contained, one of 'tree', ,
+        'events', '1D', '2D', etc.
+    - filetype: the file extension and file format used to serialize the data.
+
+    Args:
+        fpath (str): File system path to downloaded file
+
+    Returns:
+        dict: Dictionary with the fields described above.
+    """
+    fname, ext = basename(fpath).split('.')
+    segments = fname.split('_')
+    return dict(
+        participant_scope='single',
+        task_scope='single',
+        participant=segments[-3],
+        task_index=int(segments[-2]),
+        version=segments[3].replace('v', ''),
+        experiment_name=segments[1],
+        structure=segments[-1],
+        filetype=ext
     )
--- tests/test_io_meadows.py
+++ tests/test_io_meadows.py
@@ -25,3 +25,21 @@ class MeadowsIOTests(unittest.TestCase):
             rdms.dissimilarities[:2],
             [0.00791285387561264, 0.00817090931233484]
         )
+
+    def test_extract_filename_segments_1p_1t(self):
+        """Test interpretation of the filename of a Meadows results download
+
+        This case covers a filename for a single participant, single task,
+        downloaded in 2019.
+        """
+        from pyrsa.io.meadows import extract_filename_segments
+        fname = 'Meadows_myExp_v_v1_cuddly-bunny_3_1D.mat'
+        info = extract_filename_segments(fname)
+        self.assertEqual(info.get('participant_scope'), 'single')
+        self.assertEqual(info.get('task_scope'), 'single')
+        self.assertEqual(info.get('participant'), 'cuddly-bunny')
+        self.assertEqual(info.get('task_index'), 3)
+        self.assertEqual(info.get('version'), '1')
+        self.assertEqual(info.get('experiment_name'), 'myExp')
+        self.assertEqual(info.get('structure'), '1D')
+        self.assertEqual(info.get('filetype'), 'mat')
