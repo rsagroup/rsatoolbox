ff73a7d5184310db54cb6e5427fae3963943cdf6
Jasper van den Bosch <japsai@gmail.com>
2020-07-21 14:24:54 +0200

    draft of first acceptance test

---
+++ tests/test_io_meadows.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class MeadowsIOTests(unittest.TestCase):
+    """Acceptance and unit tests for loading Meadows data.
+    """
+
+    def test_load_dataset_from_mat_file(self):
+        """Acceptance test for loading data from a Meadows
+        .mat file download containing data for a single task,
+        single participant. Should have descriptors and dissimilarities
+        as found in file.
+        """
+        self.fail('unfinished test')
