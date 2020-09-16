be3a8eb577cb19ea943bf4603ed9fbd59f8ec041
Baihan Lin <nature@gatech.edu>
2020-09-16 09:29:25 -0400

    [FEAT] update vis init

--- pyrsa/vis/mds_plot.py
+++ pyrsa/vis/mds_plot.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 """
 Definition of visualization methods
+
++ rdm_dimension_reduction:  dimension reduction of RDMs class 
++ mds:  multi-dimensional scaling of RDMs class
++ weighted_mds:  weighted multi-dimensional scaling of RDMs class
+
 @author: baihan
 """
 
