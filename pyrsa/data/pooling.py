de9fd8e83c996490849b0b046b266174a577861f
Alex <adkipnis@mailbox.org>
2020-09-18 00:11:53 +0200

    added docstrings and function to load SPM beta coefficients optionally using dictionary to subset beta coefficients

--- pyrsa/data/pooling.py
+++ pyrsa/data/pooling.py
@@ -7,15 +7,32 @@ Data preparation functions
 import os
 import glob
 import numpy as np
+from nibabel import nifti1
 
-def flatten_list(lst):
-    assert isinstance(lst, list)
-    lst_flattened = [item for sublist in lst for item in sublist]
-    return lst_flattened
+
 
 class BidsDerivatives:
+    """
+    BidsDerivatives class.
+    Parses a specified subdirectory in the derivatives folder
+    of a dataset in BIDS format.
+
+    Args:
+        fmri_dir (str): path to derivatives subdirectory
+        subject_list (list): names of contained subject folders 
+        sessions (list): names of unique sessions
+        run_dirs (list): paths to all run folders containing e.g. GLM results
+
+    Returns:
+        BidsDerivatives object
+    """
+    
     def __init__(self, fmri_dir, subject_list = None, sessions = None,
                  run_dirs = None):
+        """
+        Parses BIDS structured data in fmri_dir
+        or inherits said info after subsetting
+        """
         assert os.path.exists(fmri_dir), "Specified directory does not exist."
         self.fmri_dir = fmri_dir
         
@@ -40,7 +57,6 @@ class BidsDerivatives:
             bases = [os.path.basename(sess_dir) for sess_dir
                      in session_dirs]
             self.sessions = np.unique(bases)
-        
         self.session_types = np.unique(
             [''.join([i for i in base if not i.isdigit()])
              for base in self.sessions])
@@ -57,7 +73,7 @@ class BidsDerivatives:
     
     def __repr__(self):
         """
-        defines string which is printed for the object
+        Defines string which is printed for the object
         """
         return (f'pyrsa.data.{self.__class__.__name__}(\n\n'
                 f'fMRI directory = \n{self.fmri_dir}\n\n'
@@ -69,8 +85,16 @@ class BidsDerivatives:
                 )
     
     def subset_subject(self, sub):
+        """
+        Creates a smaller BidsDerivatives object for specified subject
         
+        Args:
+            sub (int or str): subject id (e.g. 1 or 'sub-01')
+
+        Returns:
+            Subsetted BidsDerivatives object
         
+        """
         assert isinstance(sub, int) or sub in self.subject_list, \
             "Subject number must be integer or a string for an existing \
                 subject ID"
@@ -88,8 +112,19 @@ class BidsDerivatives:
         return subset
     
     def subset_session_type(self, session_type):
-        assert session_type in self.sessions, \
-            "Session type does not exist"
+        """
+        Creates a smaller BidsDerivatives object for specified session
+        
+        Args:
+            sub (int or str): subject id (e.g. 1 or 'sub-01')
+
+        Returns:
+            Subsetted BidsDerivatives object
+        
+        """
+        assert session_type in self.sessions \
+            or session_type in self.session_types, \
+            "Session (type) does not exist"
         run_dirs_subset = [run for run in self.run_dirs if session_type in run]
         subset = BidsDerivatives(self.fmri_dir,
                                  subject_list = self.subject_list,
@@ -105,6 +140,58 @@ class BidsDerivatives:
     
     def get_runs(self):
         return self.run_dirs.copy() 
-            
-        
+
+
+def flatten_list(lst):
+    """
+    Flattens list of lists to single list
+
+    Args:
+        lst (list)
+
+    Returns:
+        lst_flattened (list): the flattened version of lst
+    """
+    assert isinstance(lst, list)
+    lst_flattened = [item for sublist in lst for item in sublist]
+    return lst_flattened
+
+
+def load_SPM_beta_images(run_dirs, stim_ids_dict):
+    """
+    Collects 3d images of prespecified beta coefficients
+    (typical SPM GLM results) and corresponding metadata
+    (condition + run info) into respective lists
+
+    Args:
+        run_dirs (list of str):
+            paths to directories containing beta NIfTi files
+        stim_ids_dict (dict): {condition : beta coefficient number}
+            e.g. {'face': 1, 'house': 2}
+
+    Returns:
+        beta_array_superset (list of 3d numpy arrays):
+            all beta 3d arrays for the keys in stim_ids_dict found in each
+            run directory
+        dim4_descriptors (list of str):
+            corresponding descriptors
+            e.g. ['cond_face_run_05', 'cond_house_run_30']
+    """
+    beta_array_superset = []
+    dim4_descriptors = []
+    run_counter = 0
+    for glm_dir in run_dirs:
+        run_counter += 1
+        for condition in stim_ids_dict.keys():
+            num = stim_ids_dict[condition]
+            beta_image_path = os.path.join(glm_dir, "beta_" +
+                                           str(num).zfill(4))
+            beta_image = nifti1.load(beta_image_path)
+            beta_array_superset.append(beta_image.get_fdata())
+            dim4_descriptors.append("cond_" + condition + "_run_" +
+                                    str(run_counter).zfill(2))
+    return beta_array_superset, dim4_descriptors
+
+
+
 
