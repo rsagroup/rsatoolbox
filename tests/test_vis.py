f461ae2b4f4400c66f2dd2e53ffa6944ce288501
Heiko Schuett <heiko.schuett@nyu.edu>
2020-09-17 16:13:07 +0200

    first test fix, but the results are not actually equal

--- tests/test_vis.py
+++ tests/test_vis.py
@@ -35,7 +35,7 @@ class TestVIS(unittest.TestCase):
 
     def test_vis_weighted_mds_output_shape_corresponds_to_inputs(self):
         dis = np.random.rand(8, 10)
-        wes = np.random.random((8,10))
+        wes = np.random.random((8, 10))
         mes = "Euclidean"
         des = {'session': 0, 'subj': 0}
         rdms = rsr.RDMs(dissimilarities=dis,
@@ -46,18 +46,20 @@ class TestVIS(unittest.TestCase):
 
     def test_vis_3d_weighted_mds_output_shape_corresponds_to_inputs(self):
         dis = np.random.rand(8, 10)
-        wes = np.random.random((8,10))
+        wes = np.random.random((8, 10))
         mes = "Euclidean"
         des = {'session': 0, 'subj': 0}
         rdms = rsr.RDMs(dissimilarities=dis,
                         dissimilarity_measure=mes,
                         descriptors=des)
-        mds_emb = rsv.mds(rdms,dim=3, weight=wes)
-        self.assertEqual(mds_emb.shape, (8, 5, 3))
+        mds_emb = rsv.mds(rdms, dim=3, weight=wes)
+        self.assertEqual(mds_emb.shape[0], 8)
+        self.assertEqual(mds_emb.shape[1], 5)
+        self.assertEqual(mds_emb.shape[2], 3)
 
     def test_vis_weighted_mds_output_behaves_like_mds(self):
         dis = np.random.rand(8, 10)
-        wes = np.ones((8,10))
+        wes = np.ones((8, 10))
         mes = "Euclidean"
         des = {'session': 0, 'subj': 0}
         rdms = rsr.RDMs(dissimilarities=dis,
@@ -65,19 +67,19 @@ class TestVIS(unittest.TestCase):
                         descriptors=des)
         mds_emb = rsv.mds(rdms)
         wmds_emb = rsv.mds(rdms, weight=wes)
-        self.assertEqual(mds_emb, wmds_emb)
+        np.testing.assert_allclose(mds_emb, wmds_emb)
 
     def test_vis_3d_weighted_mds_output_behaves_like_mds(self):
         dis = np.random.rand(8, 10)
-        wes = np.ones((8,10))
+        wes = np.ones((8, 10))
         mes = "Euclidean"
         des = {'session': 0, 'subj': 0}
         rdms = rsr.RDMs(dissimilarities=dis,
                         dissimilarity_measure=mes,
                         descriptors=des)
-        mds_emb = rsv.mds(rdms,dim=3)
-        wmds_emb = rsv.mds(rdms,dim=3, weight=wes)
-        self.assertEqual(mds_emb, wmds_emb)
+        mds_emb = rsv.mds(rdms, dim=3)
+        wmds_emb = rsv.mds(rdms, dim=3, weight=wes)
+        np.testing.assert_allclose(mds_emb, wmds_emb)
 
 if __name__ == '__main__':
     unittest.main()
