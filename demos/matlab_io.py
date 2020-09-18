8576207af3932ab2b6ae69b5546f9842542b2ad0
Johan Carlin <johan.carlin@gmail.com>
2020-09-18 15:11:47 +0100

    rdm loading for neuron 2008

--- demos/matlab_io.py
+++ demos/matlab_io.py
@@ -10,6 +10,7 @@ import os
 import os.path
 import scipy.io
 from pyrsa import vis
+from pyrsa import rdm
 
 DEMO_DIR = os.path.dirname(os.path.realpath("__file__"))
 
@@ -23,3 +24,22 @@ def neuron_2008_images():
     return [
         vis.Icon(image=this_image) for this_image in mat["stimuli_92objs"][0]["image"]
     ]
+
+
+def neuron_2008_rdms_fmri():
+    """ Load Kriegeskorte et al. (2008, Neuron) fMRI RDMs as RDMs instance."""
+    mat_path = os.path.join(NEURON_DIR, "92_brainRDMs.mat")
+    mat = scipy.io.loadmat(mat_path)
+    # insert leading dim to conform with pyrsa nrdm x ncon x ncon convention
+    return rdm.concat(
+        [
+            rdm.RDMs(
+                dissimilarities=this_rdm["RDM"][None, :, :],
+                dissimilarity_measure="pearson",
+                rdm_descriptors=dict(
+                    zip(["ROI", "subject", "session"], this_rdm["name"][0].split(" | "))
+                ),
+            )
+            for this_rdm in mat["RDMs"].flatten()
+        ]
+    )
