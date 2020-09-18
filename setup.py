d1893cc92d75b87ae1b5f8cd13216ef35429d467
Jasper van den Bosch <japsai@gmail.com>
2020-09-18 21:25:06 +0200

    started the commandline entrypoint

--- setup.py
+++ setup.py
@@ -32,4 +32,7 @@ setup(
     install_requires=requires,
     tests_require=requires+['parameterized',],
     test_suite="tests",
+    entry_points = {
+        'console_scripts': ['rsa3=pyrsa.io.commandline:main'],
+    }
 )
