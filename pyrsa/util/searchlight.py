427899cc84725e6a12bf68ec4a3c74fff8600cd3
lajnd <dnllndh@gmail.com>
2020-09-18 14:14:28 +0200

    added functions for getting the searchlights in volume

--- pyrsa/util/searchlight.py
+++ pyrsa/util/searchlight.py
@@ -2,52 +2,86 @@ import numpy as np
 from scipy.spatial.distance import cdist
 from tqdm import tqdm
 from joblib import Parallel, delayed
+import nibabel as nib
 
-
-"""pySearchlight
-Returns:
-    [SL]: Searchlight object and its methods.
+"""
 This class was initially inspired by the following :
 https://github.com/machow/pysearchlight
 """
 
-def upper_tri_indexing(RDM):
-    """upper_tri_indexing returns the upper triangular index of an RDM
+data = np.load('/Users/daniel/Dropbox/amster/github/fmri_data/singe_trial_betas.npy')
+events = np.load('/Users/daniel/Dropbox/amster/github/fmri_data/singe_trial_events.npy')
+mask_img = nib.load('/Users/daniel/Dropbox/amster/github/fmri_data/sub-01_ses-01_task-WM_run-1_bold_space-MNI152NLin2009cAsym_brainmask.nii.gz')
+mask = mask_img.get_fdata()
+
+
+
+def _get_searchlight_neighbors(mask, center, radius=3):
+    """Return indices for searchlight where distance 
+        between a voxel and their center < radius (in voxels)
     
     Args:
-        RDM 2Darray: squareform RDM
+        center (index):  point around which to make searchlight sphere
     
     Returns:
-        1D array: upper triangular vector of the RDM
+        list: the list of volume indices that respect the 
+                searchlight radius for the input center.  
     """
-    # returns the upper triangle
-    m = RDM.shape[0]
-    r, c = np.triu_indices(m, 1)
-    return RDM[r, c]
+    center = np.array(center)
+    mask_shape = mask.shape
+    cx, cy, cz = np.array(center)
+    x = np.arange(mask_shape[0])
+    y = np.arange(mask_shape[1])
+    z = np.arange(mask_shape[2])
 
+    # First mask the obvious points
+    # - may actually slow down your calculation depending.
+    x = x[abs(x-cx) < radius]
+    y = y[abs(y-cy) < radius]
+    z = z[abs(z-cz) < radius]
 
-def get_rdm(data, ind):
+    # Generate grid of points
+    X, Y, Z = np.meshgrid(x, y, z)
+    data = np.vstack((X.ravel(), Y.ravel(), Z.ravel())).T
+    distance = cdist(data, center.reshape(1, -1), 'euclidean').ravel()
 
-    """get_rdm returns the correlation distance 
-       across all pairs of condition patterns in X
-       get_rdm uses numpy's einsum
-    
+    return data[distance < radius].T.tolist()
+
+def get_volume_searchLight(mask, radius=2, threshold=1):
+    """Searches through the non-zero voxels of the mask, selects centers where 
+        proportion of sphere voxels >= self.threshold.
+
     Args:
-        data (array): conditions x all channels.
-        ind (vector): subspace of voxels to index data with.
-    
+        mask ([numpy array]): [description]
+        radius (int, optional): [description]. Defaults to 2.
+        threshold (int, optional): [description]. Defaults to 1.
+
     Returns:
-        UTV: pairwise distances between condition patterns in indexed data. 
-             (in upper triangular vector form)
+        [numpy array]: array of centers of size n_centers x 3
+        [list]: list of lists with neighbors - the length of the list will correspond to:
+                len(good_neighbors) = n_centers
+                len(good_neighbors[0]) = 3
+                len(good_neighbors[0][0]) = n_neighbors for 
     """
-    ind = np.array(ind)
-    X = np.array(data[ind,:]).T
-    X = X - X.mean(axis=1, keepdims=True)
-    X /= np.sqrt(np.einsum('ij,ij->i', X, X))[:, None]
 
-    return 1 - upper_tri_indexing(np.einsum('ik,jk', X, X))
+    centers = list(zip(*np.nonzero(mask)))
+    good_centers = []
+    good_neighbors = []
 
-class RSASearchLight():
+    for center in tqdm(centers, desc='Finding searchlights...'):
+        neighbors = _get_searchlight_neighbors(mask, center, radius)
+        if mask[neighbors].mean() >= threshold:
+            good_centers.append(center)
+            good_neighbors.append(neighbors)
+
+    assert good_centers.shape[0] == len(good_neighbors), "number of centers and sets of neighbors do not match"
+    print(f'Found {len(good_neighbors)} searchlights')
+
+    return np.array(good_centers), good_neighbors
+
+    
+
+class VolumeSearchLight():
     def __init__(self, mask, radius=2, threshold=.7, njobs=1, verbose=False):
         """[summary]
         
