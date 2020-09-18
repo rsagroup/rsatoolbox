32f19fe7f44a57c5992e73218e292c33e5e465ea
Cai <mail@caiwingfield.com>
2020-09-18 20:15:17 +0100

    Function for converting selectors or indices to category names to condition indices.

--- pyrsa/util/rdm_utils.py
+++ pyrsa/util/rdm_utils.py
@@ -5,6 +5,8 @@
 @author: baihan
 """
 
+from typing import Union, List, Tuple, Dict
+
 import numpy as np
 from scipy.spatial.distance import squareform
 
@@ -96,3 +98,71 @@ def add_pattern_index(rdms, pattern_descriptor):
     pattern_select = rdms.pattern_descriptors[pattern_descriptor]
     pattern_select = np.unique(pattern_select)
     return pattern_descriptor, pattern_select
+
+
+def category_selector_to_names_and_idxs(rdms,
+                                        category_selector: Union[str, List[int]]
+                                        ) -> Tuple[List[str], Dict[str, List[int]]]:
+    """
+
+
+    Args:
+        rdms (pyrsa.rdm.RDMs):
+            A reference RDM stack.
+        category_selector (str or List[int]):
+            Either: a string specifying the `rdms.pattern_descriptor` which labels categories for each condition.
+            Or: a list of ints specifying the category label for each condition in `rdms`.
+
+    Returns:
+        a tuple of:
+            category_names (List[int]):
+                The names of the unique pattern descriptors within the descriptor specified by `category_selector`,
+                if it is a string. Otherwise generated to be "Category i" for integers i specified in
+                `category_selector`.
+            category_idxs (Dict[str, List[int]]):
+                A dictionary mapping the strings in `category_names` to lists of integer indices of categories within
+                the RDMs.
+
+    @author: caiw
+    """
+
+    from pyrsa.rdm import RDMs
+    rdms: RDMs
+
+    _msg_arg_category_selector = ("Argument category_selector must be a string specifying a pattern_descriptor or "
+                                  "a list of ints indicating RDM conditions.")
+
+    # One unique name for each category
+    category_names: List[str]
+    # Dictionary maps category names to lists of condition indices
+    condition_idxs: Dict[str, List[int]]
+
+    if isinstance(category_selector, str):
+        # Use a set to get unique category labels
+        category_names = sorted(set(rdms.pattern_descriptors[category_selector]))
+        condition_idxs = {
+            category_name: [
+                idx
+                for idx, cat in enumerate(rdms.pattern_descriptors[category_selector])
+                if cat == category_name
+            ]
+            for category_name in category_names
+        }
+
+    elif isinstance(category_selector, list) and all(isinstance(i, int) for i in category_selector):
+        if len(category_selector) != rdms.n_cond:
+            raise ValueError(_msg_arg_category_selector)
+        condition_idxs = {
+            f"Category {category_i}": [
+                idx
+                for idx, cat in enumerate(category_selector)
+                if cat == category_i
+            ]
+            for category_i in category_selector
+        }
+        category_names = sorted(condition_idxs.keys())
+
+    else:
+        raise ValueError(_msg_arg_category_selector)
+
+    return category_names, condition_idxs
--- tests/test_util_rdm.py
+++ tests/test_util_rdm.py
@@ -30,3 +30,39 @@ class TestRdmUtils(unittest.TestCase):
         assert y.shape[2] == 5
         assert n_rdm == 8
         assert n_cond == 5
+
+    def test_category_selector_to_names_and_idxs_2_categories_by_name(self):
+        from pyrsa.util.rdm_utils import category_selector_to_names_and_idxs
+        from pyrsa.rdm.rdms import RDMs
+
+        n_rdm, n_cond = 4, 8
+        dis = np.zeros((n_rdm, n_cond, n_cond))
+        rdms = RDMs(dissimilarities=dis,
+                    # 2 categories, P and Q
+                    pattern_descriptors={'type': list('PPPPQQQQ')},
+                    dissimilarity_measure='Euclidean',
+                    descriptors={'subj': range(n_rdm)})
+        category_names, condition_idxs = category_selector_to_names_and_idxs(rdms, 'type')
+
+        self.assertEqual(category_names, ['P', 'Q'])  # Names correct
+        self.assertEqual(condition_idxs, {  # Indices correct
+            'P': [0, 1, 2, 3],
+            'Q': [4, 5, 6, 7],
+        })
+
+    def test_category_selector_to_names_and_idxs_2_categories_by_ints(self):
+        from pyrsa.util.rdm_utils import category_selector_to_names_and_idxs
+        from pyrsa.rdm.rdms import RDMs
+
+        n_rdm, n_cond = 4, 8
+        dis = np.zeros((n_rdm, n_cond, n_cond))
+        rdms = RDMs(dissimilarities=dis,
+                    dissimilarity_measure='Euclidean',
+                    descriptors={'subj': range(n_rdm)})
+        category_names, condition_idxs = category_selector_to_names_and_idxs(rdms, [1, 2, 1, 2, 1, 2, 1, 2])
+
+        self.assertEqual(category_names, ['Category 1', 'Category 2'])  # Names correct
+        self.assertEqual(condition_idxs, {  # Indices correct
+            'Category 1': [0, 2, 4, 6],
+            'Category 2': [1, 3, 5, 7],
+        })
