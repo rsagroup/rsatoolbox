0eaeea43ba7dca13f5ad39b71d7361a2a74fa36e
Jasper van den Bosch <japsai@gmail.com>
2020-09-28 11:46:42 +0200

    [style] fixed style issues in meg preproc script

--- demos/TemporalSampleData/preproc_mne_sample_data.py
+++ demos/TemporalSampleData/preproc_mne_sample_data.py
@@ -1,5 +1,6 @@
+""" Script to preprocess MEG sample data for Temporal RSA demo notebook
+"""
 import pickle
-
 import mne
 from mne.datasets import sample
 
@@ -10,7 +11,7 @@ raw_fname = data_path + '/MEG/sample/sample_audvis_raw.fif'
 tmin, tmax = -0.200, .7500
 event_id = {'Auditory/Left': 1, 'Auditory/Right': 2,
             'Visual/Left': 3, 'Visual/Right': 4}
-            
+
 raw = mne.io.read_raw_fif(raw_fname, preload=True)
 
 raw.filter(1, 20)
@@ -30,8 +31,8 @@ epochs.apply_baseline((-.2,.0))
 X = epochs.get_data()  # MEG signals: n_epochs, n_meg_channels, n_times
 y = epochs.events[:, 2]
 
-pickle.dump({'data': X[:,:,:], 
-             'times': epochs.times[:], 
+pickle.dump({'data': X[:,:,:],
+             'times': epochs.times[:],
              'cond_idx': epochs.events[:,2],
              'cond_names': event_id,
              'channel_names': epochs.ch_names},
