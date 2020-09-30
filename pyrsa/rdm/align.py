9b3ab47ce8aa9573cb8710d56818a8817595d570
Jasper van den Bosch <japsai@gmail.com>
2020-09-30 17:31:29 +0200

    docstring for _mean() helper function

--- pyrsa/rdm/align.py
+++ pyrsa/rdm/align.py
@@ -1,7 +1,21 @@
+"""Private implementations of align() and mean()
+"""
 import numpy as np
 
 
 def _mean(vectors, weights=None):
+    """Weighted mean of RDM vectors
+
+    See :meth:`pyrsa.rdm.rdms.RDMs.mean`
+
+    Args:
+        vectors (ndarray): dissimilarity vectors of shape (nrdms, nconds)
+        weights (ndarray, optional): Same shape as vectors.
+            Defaults to None.
+
+    Returns:
+        ndarray: Average vector of shape (nconds,)
+    """
     if weights is None:
         weights = np.ones(vectors.shape)
         weights[np.isnan(vectors)] = np.nan
