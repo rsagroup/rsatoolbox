b47a3e8e774a1429a2e6c64bd0ea166498e9762b
Heiko Schuett <heiko.schuett@nyu.edu>
2020-10-02 23:23:04 -0400

    full revert to none in noise_ceil_var

--- pyrsa/inference/evaluate.py
+++ pyrsa/inference/evaluate.py
@@ -118,7 +118,7 @@ def eval_fixed(models, data, theta=None, method='cosine'):
         data, method=method, rdm_descriptor='index')
     variances = np.cov(evaluations[0], ddof=1) \
         / evaluations.shape[-1]
-    noise_ceil_var = np.zeros((evaluations.shape[1] + 2, 2))
+    noise_ceil_var = None
     dof = evaluations.shape[-1] - 1
     result = Result(models, evaluations, method=method,
                     cv_method='fixed', noise_ceiling=noise_ceil,
@@ -191,7 +191,7 @@ def eval_bootstrap(models, data, theta=None, method='cosine', N=1000,
         noise_ceil = np.array(boot_noise_ceiling(
             data, method=method, rdm_descriptor=rdm_descriptor))
         variances = np.cov(evaluations.T)
-        noise_ceil_var = np.zeros((evaluations.shape[1] + 2, 2))
+        noise_ceil_var = None
     dof = min(data.n_rdm, data.n_cond) - 1
     result = Result(models, evaluations, method=method,
                     cv_method='bootstrap', noise_ceiling=noise_ceil,
@@ -264,7 +264,7 @@ def eval_bootstrap_pattern(models, data, theta=None, method='cosine', N=1000,
         noise_ceil = np.array(boot_noise_ceiling(
             data, method=method, rdm_descriptor=rdm_descriptor))
         variances = np.cov(evaluations.T)
-        noise_ceil_var = np.zeros((evaluations.shape[1] + 2, 2))
+        noise_ceil_var = None
     dof = data.n_cond - 1
     result = Result(models, evaluations, method=method,
                     cv_method='bootstrap_pattern', noise_ceiling=noise_ceil,
@@ -321,7 +321,7 @@ def eval_bootstrap_rdm(models, data, theta=None, method='cosine', N=1000,
         noise_ceil = np.array(boot_noise_ceiling(
             data, method=method, rdm_descriptor=rdm_descriptor))
         variances = np.cov(evaluations.T)
-        noise_ceil_var = np.zeros((evaluations.shape[1] + 2, 2))
+        noise_ceil_var = None
     dof = data.n_rdm - 1
     variances = np.cov(evaluations.T)
     result = Result(models, evaluations, method=method,
