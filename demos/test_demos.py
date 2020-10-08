caf0b79e83ac95c3078646adc9a2a2ebf4772bea
Johan Carlin <johan.carlin@gmail.com>
2020-10-08 10:21:14 +0100

    demo test code in progress

---
+++ demos/test_demos.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+test_demos
+Test for demo notebooks and matlab_io module
+@author: jooh
+"""
+
+import 
+
+class Test_matlab_io(unittest.TestCase):
+
+    def test_neuron_2008_images():
+
+import unittest
+
+if __name__ == '__main__':
+    unittest.main()
