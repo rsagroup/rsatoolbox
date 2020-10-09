2cfb003e8dd4433e995218c7ecb89423a527007a
lajnd <dnllndh@gmail.com>
2020-10-09 14:01:50 +0200

    fixed blank spaces

--- pyrsa/util/searchlight.py
+++ pyrsa/util/searchlight.py
@@ -15,15 +15,15 @@ https://github.com/machow/pysearchlight
 
 
 def _get_searchlight_neighbors(mask, center, radius=3):
-    """Return indices for searchlight where distance 
+    """Return indices for searchlight where distance
         between a voxel and their center < radius (in voxels)
-    
+
     Args:
         center (index):  point around which to make searchlight sphere
-    
+
     Returns:
-        list: the list of volume indices that respect the 
-                searchlight radius for the input center.  
+        list: the list of volume indices that respect the
+                searchlight radius for the input center.
     """
     center = np.array(center)
     mask_shape = mask.shape
@@ -34,9 +34,9 @@ def _get_searchlight_neighbors(mask, center, radius=3):
 
     # First mask the obvious points
     # - may actually slow down your calculation depending.
-    x = x[abs(x-cx) < radius]
-    y = y[abs(y-cy) < radius]
-    z = z[abs(z-cz) < radius]
+    x = x[abs(x - cx) < radius]
+    y = y[abs(y - cy) < radius]
+    z = z[abs(z - cz) < radius]
 
     # Generate grid of points
     X, Y, Z = np.meshgrid(x, y, z)
@@ -45,16 +45,19 @@ def _get_searchlight_neighbors(mask, center, radius=3):
 
     return tuple(data[distance < radius].T.tolist())
 
+
 def get_volume_searchlight(mask, radius=2, threshold=1.0):
-    """Searches through the non-zero voxels of the mask, selects centers where 
+    """Searches through the non-zero voxels of the mask, selects centers where
         proportion of sphere voxels >= self.threshold.
 
     Args:
         mask ([numpy array]): binary brain mask
         radius (int, optional): the radius of each searchlight, defined in voxels. Defaults to 2.
-        threshold (float, optional): Threshold of the proportion of voxels that need to be inside the brain mask
-                                     in order for it to be considered a good searchlight center.
-                                     Values go between 0.0 - 1.0 where 1.0 means that 100% of the voxels need to be inside
+        threshold (float, optional): Threshold of the proportion of voxels that need to
+                                     be inside the brain mask in order for it to be
+                                     considered a good searchlight center.
+                                     Values go between 0.0 - 1.0 where 1.0 means that
+                                     100% of the voxels need to be inside
                                      the brain mask. Defaults to 1.0.
 
     Returns:
@@ -77,7 +80,8 @@ def get_volume_searchlight(mask, radius=2, threshold=1.0):
             good_neighbors.append(neighbors)
 
     good_centers = np.array(good_centers)
-    assert good_centers.shape[0] == len(good_neighbors), "number of centers and sets of neighbors do not match"
+    assert good_centers.shape[0] == len(good_neighbors),\
+        "number of centers and sets of neighbors do not match"
     print(f'Found {len(good_neighbors)} searchlights')
 
     # turn the 3-dim coordinates to array coordinates
@@ -86,22 +90,25 @@ def get_volume_searchlight(mask, radius=2, threshold=1.0):
 
     return centers, neighbors
 
+
 def get_searchlight_RDMs(data_2d, centers, neighbors, events,
-                        method='correlation', verbose=True):
-    """Iterates over all the searchlight centers and calculates the RDM 
+                         method='correlation', verbose=True):
+    """Iterates over all the searchlight centers and calculates the RDM
 
     Args:
         data_2d (2D numpy array): brain data, shape n_observations x n_channels (i.e. voxels/vertices)
-        centers (1D numpy array): center indices for all searchlights as provided 
+        centers (1D numpy array): center indices for all searchlights as provided
                                         by pyrsa.util.searchlight.get_volume_searchlight
-        neighbors (list): list of lists with neighbor voxel indices for all searchlights 
+        neighbors (list): list of lists with neighbor voxel indices for all searchlights
                                         as provided by pyrsa.util.searchlight.get_volume_searchlight
         events (1D numpy array): 1D array of length n_observations
-        method (str, optional): distance metric, see pyrsa.rdm.calc for options. Defaults to 'correlation'.
+        method (str, optional): distance metric,
+                                see pyrsa.rdm.calc for options. Defaults to 'correlation'.
         verbose (bool, optional): Defaults to True.
 
     Returns:
-        RDM [pyrsa.rdm.RDMs]: RDMs object with the RDM for each searchlight, the RDM.rdm_descriptors['voxel_index']
+        RDM [pyrsa.rdm.RDMs]: RDMs object with the RDM for each searchlight
+                              the RDM.rdm_descriptors['voxel_index']
                               describes the center voxel index each RDM is associated with
     """
 
@@ -113,26 +120,27 @@ def get_searchlight_RDMs(data_2d, centers, neighbors, events,
         # we can't run all centers at once, that will take too much memory
         # so lets to some chunking
         chunked_center = np.split(np.arange(n_centers),
-                                np.linspace(0, n_centers,
-                                101, dtype=int)[1:-1])
-        
+                                  np.linspace(0, n_centers,
+                                              101, dtype=int)[1:-1])
+
         # loop over chunks
         n_conds = len(np.unique(events))
-        RDM = np.zeros((n_centers, n_conds * (n_conds-1) // 2))
-        for chunk in tqdm(chunked_center, desc='Calculating RDMs...'):
+        RDM = np.zeros((n_centers, n_conds * (n_conds - 1) // 2))
+        for chunks in tqdm(chunked_center, desc='Calculating RDMs...'):
             center_data = []
-            for c in chunk:
+            for c in chunks:
                 # grab this center and neighbors
                 center = centers[c]
-                nb = neighbors[c]
+                center_neighbors = neighbors[c]
                 # create a database object with this data
-                ds = Dataset(data_2d[:, nb],
-                            descriptors={'center': c},
-                            obs_descriptors={'events':events},
-                            channel_descriptors={'voxels': nb})
+                ds = Dataset(data_2d[:, center_neighbors],
+                             descriptors={'center': c},
+                             obs_descriptors={'events': events},
+                             channel_descriptors={'voxels': nb})
                 center_data.append(ds)
 
-            RDM_corr = calc_rdm(center_data, method=method, descriptor='events')
+            RDM_corr = calc_rdm(center_data, method=method,
+                                descriptor='events')
             RDM[chunk, :] = RDM_corr.dissimilarities
     else:
         center_data = []
@@ -142,20 +150,20 @@ def get_searchlight_RDMs(data_2d, centers, neighbors, events,
             nb = neighbors[c]
             # create a database object with this data
             ds = Dataset(data_2d[:, nb],
-                        descriptors={'center': c},
-                        obs_descriptors={'events':events},
-                        channel_descriptors={'voxels': nb})
+                         descriptors={'center': c},
+                         obs_descriptors={'events': events},
+                         channel_descriptors={'voxels': nb})
             center_data.append(ds)
         # calculate RDMs for each database object
-        RDM = calc_rdm(center_data, method=method, descriptor='events').dissimilarities
-    
-
+        RDM = calc_rdm(center_data, method=method,
+                       descriptor='events').dissimilarities
 
     SL_rdms = RDMs(RDM,
-                      rdm_descriptors={'voxel_index':centers},
-                      dissimilarity_measure=method)
+                   rdm_descriptors={'voxel_index': centers},
+                   dissimilarity_measure=method)
 
     return SL_rdms
+
 
 def evaluate_models_searchlight(sl_RDM, models, eval_function, method='corr', theta=None, n_jobs=1):
     """evaluates each searchlighth with the given model/models
@@ -172,7 +180,8 @@ def evaluate_models_searchlight(sl_RDM, models, eval_function, method='corr', th
     """
 
     results = Parallel(n_jobs=n_jobs)(
-                    delayed(eval_function)(
-                        models, x, method=method, theta=theta) for x in tqdm(sl_RDM, desc='Evaluating models for each searchlight'))
+        delayed(eval_function)(
+            models, x, method=method, theta=theta) for x in tqdm(
+            sl_RDM, desc='Evaluating models for each searchlight'))
 
     return results
