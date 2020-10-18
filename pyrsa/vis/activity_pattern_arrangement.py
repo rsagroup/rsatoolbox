249d39a0495d58c5c6f1c3d24c68215acfb45813
Brandon-YuHu <66934295+Brandon-YuHu@users.noreply.github.com>
2020-10-18 13:45:54 +0800

    Update activity_pattern_arrangement.py

--- pyrsa/vis/activity_pattern_arrangement.py
+++ pyrsa/vis/activity_pattern_arrangement.py
@@ -20,9 +20,7 @@ def activity_pattern_arrangement(rdm, condition_icons, method=None):
     """
     if len(condition_icons) != rdm.n_cond:
         raise TypeError("The number of conditions in RDM does not match the number of condition icons")
-    
     rdmm = rdm.get_matrices()
-    
     if method == 'mds' or method is None:
         emb = MDS(n_components=2, dissimilarity='precomputed')
     elif method == 'isomap':
@@ -31,18 +29,14 @@ def activity_pattern_arrangement(rdm, condition_icons, method=None):
         emb = TSNE(n_components=2)
     else:
         raise TypeError("Incorrect method")
-        
     for i in np.arange(rdms.n_rdm):
         drs = emb.fit_transform(rdmm[i, :, :])
-        
         fig, ax = plt.subplots()
         fig.set_size_inches(30, 30)
         ax.set_xlim(drs.min() - 0.01, drs.max() + 0.01)
         ax.set_ylim(drs.min() - 0.01, drs.max() + 0.01)
         ax.axis('off')
-
         for j,icon in zip(np.arange(rdm.n_cond),condition_icons.values()):
             imagebox = OffsetImage(icon.image.resize((100,100)))
             ab = AnnotationBbox(imagebox, (drs[j,0],drs[j,1]), frameon=False)
             ax.add_artist(ab)
-            
