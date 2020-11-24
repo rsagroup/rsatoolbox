0db750e99d1d634f9459615ce6c5dc92aa4ad167
Heiko Schütt <heiko.schuett@nyu.edu>
2020-11-23 21:30:45 -0500

    failing test created for descriptor replication bug

--- tests/test_rdm.py
+++ tests/test_rdm.py
@@ -260,6 +260,7 @@ class TestRDM(unittest.TestCase):
                          rdm_descriptors=rdm_des)
         rdms = concat((rdms1, rdms2))
         assert rdms.n_rdm == 16
+        assert len(rdms.rdm_descriptors['session'] == 16)
 
     def test_categorical_rdm(self):
         from pyrsa.rdm import get_categorical_rdm
@@ -425,10 +426,10 @@ class TestCalcRDM(unittest.TestCase):
         )
 
     def test_calc_list_descriptors(self):
-        rdm = rsr.calc_rdm([self.test_data, self.test_data],
+        rdm = rsr.calc_rdm([self.test_data, self.test_data, self.test_data],
                            descriptor='conds',
                            method='euclidean')
-        assert np.all(rdm.rdm_descriptors['subj'] == np.array([0, 0]))
+        assert np.all(rdm.rdm_descriptors['subj'] == np.array([0, 0, 0]))
 
     def test_calc_mahalanobis(self):
         rdm = rsr.calc_rdm(self.test_data, descriptor='conds',
@@ -511,7 +512,7 @@ class TestCalcRDMMovie(unittest.TestCase):
 
     def setUp(self):
         measurements_time = np.random.rand(20, 5, 15)
-        tim_des = {'time': np.linspace(0,200, 15)}
+        tim_des = {'time': np.linspace(0, 200, 15)}
 
         des = {'session': 0, 'subj': 0}
         obs_des = {'conds': np.array([0, 0, 1, 1, 2, 2, 2, 3, 4, 5,
@@ -546,6 +547,7 @@ class TestCalcRDMMovie(unittest.TestCase):
         assert rdm.n_cond == 6
         assert len([r for r in rdm]) == 15
         assert rdm.rdm_descriptors['time'][0] == 0.0
+        assert len(rdm.rdm_descriptors['time']) == 15
 
     def test_calc_rdm_movie_euclidean(self):
         rdm = rsr.calc_rdm_movie(self.test_data_time, descriptor='conds',
