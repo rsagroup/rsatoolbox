a501ffd6a6fe7fdcd1a566bcd474e5044c472961
Johan Carlin <johan.carlin@gmail.com>
2020-11-30 16:31:40 +0000

    rdm_plot refactor with show_rdm_panel
    * rdm_plot: factor out axis-specific code to show_rdm_panel (support plotting into
    existing axis)
    * vis: use new rdm.mplstyle style sheet to control plot aesthetics (and
    support customisation)
    * show_rdm: add labels only on margins of multi-panel figures
    * show_rdm: basic auto-sizing of show_rdm figure

--- pyrsa/vis/rdm_plot.py
+++ pyrsa/vis/rdm_plot.py
@@ -4,16 +4,23 @@
 Plot showing an RDMs object
 """
 
+import os.path
+import inspect
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from pyrsa import vis
 from pyrsa.rdm import rank_transform
 from pyrsa.vis.colors import rdm_colormap
 
+MODULE_DIR = os.path.dirname(inspect.getfile(inspect.currentframe()))
 
-def show_rdm(rdm, do_rank_transform=False, pattern_descriptor=None,
-             cmap=None, rdm_descriptor=None, dpi=300, filename=None,
-             show_colorbar=False, **kwarg):
+RDM_STYLE = os.path.join(MODULE_DIR, 'rdm.mplstyle')
+
+def show_rdm(rdm, do_rank_transform=False, pattern_descriptor=None, cmap=None,
+        rdm_descriptor=None, n_column=None, n_row=None, show_colorbar=None,
+        gridlines=[], num_pattern_groups=None, figsize=None, nanmask=None,
+        style=RDM_STYLE, **kwarg):
     """shows an rdm object
 
     Parameters
@@ -29,117 +36,147 @@ def show_rdm(rdm, do_rank_transform=False, pattern_descriptor=None,
         conventions as for matplotlib colormaps
     rdm_descriptor : String
         name of a rdm descriptor which will be used as a title per RDM
-    dpi : int
-        dots per inch (determines visual resolution of plots)
-    filename : str
-        relative path to which the plot will be saved
-        (if None: do not save plot)
-    show_colorbar : bool
-        whether to display a colorbar next to each RDM
-
+    show_colorbar : str
+        whether to display a colorbar. If 'panel' a colorbar is added next to
+        each RDM. If 'figure' a shared colorbar (and scale) is used across panels.
+    nanmask : array
     """
-    plt.figure(dpi=dpi)
+
+
+    n_panel = rdm.n_rdm
+    if show_colorbar == 'figure':
+        n_panel += 1
+    if n_column is None and n_row is None:
+        n_column = np.ceil(np.sqrt(n_panel))
+        n_row = np.ceil((1 + n_panel) / n_column)
+    if n_row is None:
+        n_row = np.ceil(n_panel / n_column)
+    if n_column is None:
+        n_column = np.ceil(n_panel / n_row)
+    if (n_column * n_row) < rdm.n_rdm:
+        raise ValueError(
+                f'invalid n_row*n_column specification for {n_panel} rdms: {n_row}*{n_column}')
+    if figsize is None:
+        # scale with number of RDMs, up to a point (the intersection of A4 and us
+        # letter)
+        figsize = (min(2 * n_column, 8.3), min(2 * n_row, 11))
+    if nanmask is None:
+        nanmask = np.eye(rdm.n_cond, dtype=bool)
     if cmap is None:
         cmap = rdm_colormap()
     if do_rank_transform:
         rdm = rank_transform(rdm)
-    rdm_mat = rdm.get_matrices()
-    alpha = 1-np.eye(rdm.n_cond)
-    if rdm.n_rdm > 1:
-        m = np.ceil(np.sqrt(rdm.n_rdm+1))
-        n = np.ceil((1 + rdm.n_rdm) / m)
-        for idx in range(rdm.n_rdm):
-            plt.subplot(n, m, idx + 1)
-            image = plt.imshow(rdm_mat[idx], cmap=cmap, alpha=alpha)
-            _add_descriptor_labels(rdm, pattern_descriptor, **kwarg)
-            if rdm_descriptor in rdm.rdm_descriptors:
-                plt.title(rdm.rdm_descriptors[rdm_descriptor][idx])
-            elif isinstance(rdm_descriptor, str):
-                plt.title(rdm_descriptor)
-            if show_colorbar:
-                plt.colorbar(image)
-        plt.subplot(n, m, n * m)
-        image = plt.imshow(np.mean(rdm_mat, axis=0), cmap=cmap, alpha=alpha)
-        _add_descriptor_labels(rdm, pattern_descriptor, **kwarg)
-        plt.title('Average')
-        if show_colorbar:
-            plt.colorbar(image)
-    elif rdm.n_rdm == 1:
-        image = plt.imshow(rdm_mat[0], cmap=cmap, alpha=alpha)
-        _add_descriptor_labels(rdm, pattern_descriptor, **kwarg)
-        if rdm_descriptor in rdm.rdm_descriptors:
-            plt.title(rdm.rdm_descriptors[rdm_descriptor][0])
-        elif isinstance(rdm_descriptor, str):
-            plt.title(rdm_descriptor)
-        if show_colorbar:
-            plt.colorbar(image)
-    if isinstance(filename, str):
-        fig1 = plt.gcf()
-        fig1.savefig(filename, bbox_inches='tight')
-    plt.show()
+    if not np.any(gridlines) and num_pattern_groups:
+        # grid by pattern groups if they exist and explicit grid setting does not
+        gridlines = np.arange(num_pattern_groups-.5, rdm.n_cond+.5,
+                num_pattern_groups)
+    # we don't necessarily have the same number of RDMs as panels, so need to stop the
+    # loop when we've plotted all the RDMs
+    rdms_gen = (this_rdm for this_rdm in rdm)
+    with plt.style.context(style):
+        fig, ax_array = plt.subplots(nrows=int(n_row), ncols=int(n_column), sharex=True,
+                sharey=True, squeeze=False, figsize=figsize)
+        # reverse panel order so unfilled rows are at top instead of bottom
+        ax_array = ax_array[::-1]
+        for row_ind, row in enumerate(ax_array):
+            for col_ind, panel in enumerate(row):
+                try:
+                    image = show_rdm_panel(next(rdms_gen), ax=panel, cmap=cmap,
+                            nanmask=nanmask,
+                            rdm_descriptor=rdm_descriptor, gridlines=gridlines, **kwarg)
+                except StopIteration:
+                    # hide empty panels
+                    panel.set_visible(False)
+                    continue
+                except:
+                    raise
+                if col_ind == 0:
+                    _add_descriptor_y_labels(rdm, pattern_descriptor, ax=panel,
+                            num_pattern_groups=num_pattern_groups, **kwarg)
+                if row_ind == 0:
+                    _add_descriptor_x_labels(rdm, pattern_descriptor, ax=panel,
+                            num_pattern_groups=num_pattern_groups, **kwarg)
+                if show_colorbar == 'panel':
+                    cb = fig.colorbar(mappable=image, ax=panel, shrink=.25, aspect=5,
+                            anchor=(0., 1.),
+                            ticks=matplotlib.ticker.LinearLocator(numticks=3))
+                    cb.ax.set_title(rdm.dissimilarity_measure, loc='left',
+                            fontdict=dict(fontweight='normal'))
+    # TODO - handle show_colorbar=='figure'
+    return fig
 
-
-def _add_descriptor_labels(rdm, descriptor, num_pattern_groups=1, size=.5, offset=7,
-        linewidth=None, ax=None, axis="xy", gridlines=None):
-    """ adds a descriptor as ticklabels """
+def show_rdm_panel(rdm, ax=None, cmap=None, nanmask=None, rdm_descriptor=None,
+        gridlines=[], **kwarg):
     if ax is None:
         ax = plt.gca()
+    if rdm.n_rdm > 1:
+        raise ValueError('expected single rdm - use show_rdm for multi-panel figures')
+    rdmat = rdm.get_matrices()[0,:,:]
+    if np.any(nanmask):
+        rdmat[nanmask] = np.nan
+    image = ax.imshow(rdmat, cmap=cmap)
+    ax.set_xlim(-0.5, rdm.n_cond - 0.5)
+    ax.set_ylim(rdm.n_cond - 0.5, -0.5)
+    ax.xaxis.set_ticks(gridlines)
+    ax.yaxis.set_ticks(gridlines)
+    #ax.xaxis.set_tick_params(length=0)
+    #ax.yaxis.set_tick_params(length=0)
+    ax.xaxis.set_ticklabels([])
+    ax.yaxis.set_ticklabels([])
+    ax.xaxis.set_ticks(np.arange(rdm.n_cond), minor=True)
+    ax.yaxis.set_ticks(np.arange(rdm.n_cond), minor=True)
+    if rdm_descriptor in rdm.rdm_descriptors:
+        ax.set_title(rdm.rdm_descriptors[rdm_descriptor][0])
+    elif isinstance(rdm_descriptor, str):
+        ax.set_title(rdm_descriptor)
+    return image
+
+def _add_descriptor_x_labels(rdm, descriptor, ax=None, **kwarg):
+    if ax is None:
+        ax = plt.gca()
+    _add_descriptor_labels(rdm, descriptor, axis=ax.xaxis, other_axis=ax.yaxis,
+            horizontalalignment='center', **kwarg)
+
+def _add_descriptor_y_labels(rdm, descriptor, ax=None, **kwarg):
+    if ax is None:
+        ax = plt.gca()
+    _add_descriptor_labels(rdm, descriptor, axis=ax.yaxis, other_axis=ax.xaxis,
+            horizontalalignment='right', **kwarg)
+
+def _add_descriptor_labels(rdm, descriptor, axis, other_axis, num_pattern_groups=None,
+        size=.5, offset=7, linewidth=None, horizontalalignment='center'):
+    """ adds a descriptor as ticklabels to the axis (XAxis or YAxis instance)"""
     if linewidth is None:
         linewidth = .5
+    if num_pattern_groups is None or num_pattern_groups==0:
+        num_pattern_groups = 1
     if descriptor is not None:
         desc = rdm.pattern_descriptors[descriptor]
-        ax.set_xticks(np.arange(rdm.n_cond), minor=True)
-        ax.set_yticks(np.arange(rdm.n_cond), minor=True)
         if isinstance(desc[0], vis.Icon):
-            # image labels
+            # annotated labels with Icon
             if linewidth > 0:
-                # TODO - axis specific
-                ax.yaxis.set_tick_params(length=0, which='minor')
-                ax.xaxis.set_tick_params(length=0, which='minor')
+                axis.set_tick_params(length=0, which='minor')
             # TODO - work out sizing from pixel size transform
             for group_ind in range(num_pattern_groups, 0, -1):
                 position = offset * group_ind
                 ticks = np.arange(group_ind-1, rdm.n_cond, num_pattern_groups)
-                # TODO - let's not plot rows and columns each time
-                [this_desc.x_tick_label(this_x, size, offset=position,
-                    linewidth=linewidth) for (this_x, this_desc) in
-                        zip(ticks, desc[ticks])]
-                [this_desc.y_tick_label(this_y, size, offset=position,
-                    linewidth=linewidth) for (this_y, this_desc) in
-                        zip(ticks, desc[ticks])]
-            # grid the groups
-            if not np.any(gridlines):
-                gridlines = np.arange(num_pattern_groups-.5, rdm.n_cond+.5,
-                        num_pattern_groups)
+                if isinstance(axis, matplotlib.axis.XAxis):
+                    [this_desc.x_tick_label(this_x, size, offset=position,
+                        linewidth=linewidth, ax=axis.axes) for (this_x, this_desc) in
+                            zip(ticks, desc[ticks])]
+                elif isinstance(axis, matplotlib.axis.YAxis):
+                    [this_desc.y_tick_label(this_y, size, offset=position,
+                        linewidth=linewidth, ax=axis.axes) for (this_y, this_desc) in
+                            zip(ticks, desc[ticks])]
+                else:
+                    raise TypeError("expected axis to be XAxis or YAxis instance")
         else:
-            # vanilla
-            ax.set_xticklabels(
-                desc,
-                {'fontsize': 'xx-small',
-                 'fontweight': 'normal',
-                 'verticalalignment': 'center',
-                 'horizontalalignment': 'center'}, minor=True)
-            ax.set_yticks(np.arange(rdm.n_cond))
-            ax.set_yticklabels(
-                desc,
-                {'fontsize': 'xx-small',
-                 'fontweight': 'normal',
-                 'verticalalignment': 'center',
-                 'horizontalalignment': 'right'}, minor=True)
-            # rotate if the string is over some reasonable limit
-            # if isinstance(desc[0], str) and max([len(this_desc) for this_desc in desc]) > 5:
-                # ax.tick_params(axis='x', rotation=45, ha='right')
-        plt.ylim(rdm.n_cond - 0.5, -0.5)
-        plt.xlim(-0.5, rdm.n_cond - 0.5)
-        ax.set_xticks(gridlines)
-        ax.set_yticks(gridlines)
-        ax.yaxis.set_tick_params(length=0)
-        ax.xaxis.set_tick_params(length=0)
-        ax.set_xticklabels([])
-        ax.set_yticklabels([])
-        ax.grid(axis='both', color='w')
-        plt.setp(ax.get_xticklabels(minor=True), rotation=90, ha="right",
-                rotation_mode="anchor")
-    else:
-        ax.set_xticks([])
-        ax.set_yticks([])
+            # vanilla matplotlib-based
+            axis.set_ticklabels(
+                    desc,
+                    verticalalignment='center',
+                    horizontalalignment=horizontalalignment,
+                    minor=True)
+            if isinstance(axis, matplotlib.axis.XAxis):
+                plt.setp(axis.get_ticklabels(minor=True), rotation=60, ha="right",
+                    rotation_mode="anchor")
