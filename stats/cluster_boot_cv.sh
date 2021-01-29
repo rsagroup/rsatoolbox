3d4e8fd8cab168bc0d1cc577a9031dbd36544889
Heiko Schütt <heiko.schuett@nyu.edu>
2021-01-28 19:29:58 -0500

    right singularity overlay

--- stats/cluster_boot_cv.sh
+++ stats/cluster_boot_cv.sh
@@ -17,6 +17,6 @@ overlay_ext3=/scratch/hhs4/anaconda/overlay-25GB-500K.ext3
 
 singularity \
     exec --overlay $overlay_ext3:ro \
-    /scratch/work/public/singularity/centos-7.8.2003.sif \
+    /scratch/work/public/singularity/cuda11.1-cudnn8-devel-ubuntu18.04.sif \
     /bin/bash -c "conda activate /ext3/.env/rsa \
                   python stats.py -p /scratch/hhs4/ecoset/val boot_cv 1"
