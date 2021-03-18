42530c52c8d6431094c504f3468ed5b793360a82
Heiko Schutt <hs3110@bake.cm.cluster>
2021-03-18 19:48:50 -0400

    cluster settings updated

--- stats/cluster_allen_download.sh
+++ stats/cluster_allen_download.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=4:00:00
-#SBATCH --mem=8GB
+#SBATCH --mem-per-cpu=8GB
 #SBATCH --job-name=allen_download
 #SBATCH --mail-type=END
 #SBATCH --mail-user=hs3110@columbia.edu
-#SBATCH --output=slurm-output/slurm_allen_down_%a.out
+#SBATCH --output=/moto/nklab/users/hs3110/pyrsa/stats/slurm-output/slurm_allen_down_%a.out
 
 index=$SLURM_ARRAY_TASK_ID
 job=$SLURM_JOB_ID
 ppn=$SLURM_JOB_CPUS_PER_NODE
 
 module purge
-module load anaconda3/5.3.1
+module load shared anaconda/3-2019.10
 
-source activate RSA
+source activate rsa
 
+pwd
 which python
 
-~/.conda/envs/RSA/bin/python allen_stats.py download
+
+~/.conda/envs/rsa/bin/python allen_stats.py download
