3de5d2afd2d538ba9a729b00e1ee0c7e9b40a3c4
Heiko Schuett <heiko.schuett@nyu.edu>
2021-04-02 16:34:00 -0400

    added sqrt transform

--- pyrsa/rdm/rdms.py
+++ pyrsa/rdm/rdms.py
@@ -9,6 +9,7 @@ Definition of RSA RDMs class and subclasses
 import numpy as np
 from scipy.stats import rankdata
 from collections.abc import Iterable
+from copy import deepcopy
 from pyrsa.util.rdm_utils import batch_to_vectors
 from pyrsa.util.rdm_utils import batch_to_matrices
 from pyrsa.util.descriptor_utils import format_descriptor
@@ -463,9 +464,58 @@ def rank_transform(rdms, method='average'):
                                 for i in range(rdms.n_rdm)])
     rdms_new = RDMs(dissimilarities,
                     dissimilarity_measure=rdms.dissimilarity_measure,
-                    descriptors=rdms.descriptors,
-                    rdm_descriptors=rdms.rdm_descriptors,
-                    pattern_descriptors=rdms.pattern_descriptors)
+                    descriptors=deepcopy(rdms.descriptors),
+                    rdm_descriptors=deepcopy(rdms.rdm_descriptors),
+                    pattern_descriptors=deepcopy(rdms.pattern_descriptors))
+    return rdms_new
+
+
+def sqrt_transform(rdms):
+    """ applies a square root transform and generates a new RDMs object
+    This sets values blow 0 to 0 and takes a square root of each entry.
+    It also adds a sqrt to the dissimilarity_measure entry.
+
+    Args:
+        rdms(RDMs): RDMs object
+
+    Returns:
+        rdms_new(RDMs): RDMs object with sqrt transformed dissimilarities
+
+    """
+    dissimilarities = rdms.get_vectors()
+    dissimilarities[dissimilarities < 0] = 0
+    dissimilarities = np.sqrt(dissimilarities)
+    if rdms.dissimilarity_measure == 'squared euclidean':
+        dissimilarity_measure = 'euclidean'
+    elif rdms.dissimilarity_measure == 'squared mahalanobis':
+        dissimilarity_measure = 'mahalanobis'
+    else:
+        dissimilarity_measure = 'sqrt of' + rdms.dissimilarity_measure
+    rdms_new = RDMs(dissimilarities,
+                    dissimilarity_measure=dissimilarity_measure,
+                    descriptors=deepcopy(rdms.descriptors),
+                    rdm_descriptors=deepcopy(rdms.rdm_descriptors),
+                    pattern_descriptors=deepcopy(rdms.pattern_descriptors))
+    return rdms_new
+
+
+def positive_transform(rdms):
+    """ sets all negative entries in an RDM to zero and returns a new RDMs
+
+    Args:
+        rdms(RDMs): RDMs object
+
+    Returns:
+        rdms_new(RDMs): RDMs object with sqrt transformed dissimilarities
+
+    """
+    dissimilarities = rdms.get_vectors()
+    dissimilarities[dissimilarities < 0] = 0
+    rdms_new = RDMs(dissimilarities,
+                    dissimilarity_measure=rdms.dissimilarity_measure,
+                    descriptors=deepcopy(rdms.descriptors),
+                    rdm_descriptors=deepcopy(rdms.rdm_descriptors),
+                    pattern_descriptors=deepcopy(rdms.pattern_descriptors))
     return rdms_new
 
 
