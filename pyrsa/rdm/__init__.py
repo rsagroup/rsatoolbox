6c127c36c7de1946d1bf6ab79948d11215f0b372
Heiko Schuett <heiko.schuett@nyu.edu>
2021-04-02 17:45:43 -0400

    added squared and unsquared rdm descritption to the notebook

--- pyrsa/rdm/__init__.py
+++ pyrsa/rdm/__init__.py
@@ -2,6 +2,8 @@ from .rdms import RDMs
 from .rdms import concat
 from .rdms import get_categorical_rdm
 from .rdms import rank_transform
+from .rdms import sqrt_transform
+from .rdms import positive_transform
 from .rdms import load_rdm
 from .rdms import rdms_from_dict
 from .calc import calc_rdm
