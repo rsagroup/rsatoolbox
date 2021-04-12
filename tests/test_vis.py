7a985be792e9d745fdfb91b5ec03d8117917c080
Johan Carlin <johan.carlin@gmail.com>
2021-04-12 13:30:17 +0100

    use renamed arg color instead of col

--- tests/test_vis.py
+++ tests/test_vis.py
@@ -92,18 +92,18 @@ class Test_Icon(unittest.TestCase):
         from pyrsa.vis import Icon
         import matplotlib.pyplot as plt
         test_im = PIL.Image.fromarray(255 * np.random.rand(50, 100))
-        ic5 = Icon(image=test_im, col='red', border_width=5,
+        ic5 = Icon(image=test_im, color='red', border_width=5,
                    make_square=True, resolution=20)
         ic5.plot(0.8, 0.8)
         ic = Icon(image=255 * np.random.rand(50, 100), cmap='Blues')
         ax = plt.axes(label='test')
         ic.plot(0.5, 0.5, ax=ax)
-        ic2 = Icon(image=test_im, col='black', border_width=15,
+        ic2 = Icon(image=test_im, color='black', border_width=15,
                    string='test')
         ic2.plot(0.8, 0.2, ax=ax, size=0.4)
         ic2.x_tick_label(0.5, 0.15, offset=7)
         ic2.y_tick_label(0.5, 0.25, offset=7)
-        ic3 = Icon(image=test_im, col='red', border_width=5,
+        ic3 = Icon(image=test_im, color='red', border_width=5,
                    make_square=True)
         ic3.plot(0.2, 0.2, size=0.4)
         ic4 = Icon(string='test')
