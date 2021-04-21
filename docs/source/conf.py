4f9eec72bcfd278a94f69b8cf3eb23ebf46e4619
Jasper van den Bosch <japsai@gmail.com>
2021-04-22 01:52:48 +0200

    docs short version should only have major.minor

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, rsatoolbox authors'
 author = 'rsatoolbox authors'
 
 # The short X.Y version
-version = '0.0.1'
+version = '0.0'
 # The full version, including alpha/beta/rc tags
 release = version
 
