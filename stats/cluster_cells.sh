b1ed68c1c244908cdc8577690b03096d2e5ebcff
Heiko Schuett <heiko.schuett@nyu.edu>
2021-06-09 20:34:13 -0400

    singularity cluster

--- stats/cluster_cells.sh
+++ stats/cluster_cells.sh
@@ -7,17 +7,19 @@
 #SBATCH --job-name=cells
 #SBATCH --mail-type=END
 #SBATCH --mail-user=hhs4@nyu.edu
-#SBATCH --output=slurm-output/slurm_%a.out
+#SBATCH --output=slurm-output/slurm_cell_%a.out
 
 index=$SLURM_ARRAY_TASK_ID
 job=$SLURM_JOB_ID
 ppn=$SLURM_JOB_CPUS_PER_NODE
 
 module purge
-module load anaconda3/5.3.1
+module load anaconda3/2020.07
 
-source activate RSA
-
-which python
-
-~/.conda/envs/RSA/bin/python allen_stats.py cells
+singularity exec --overlay /scratch/hhs4/anaconda/overlay-25GB-500K.ext3:ro \
+    /scratch/work/public/singularity/cuda11.1.1-cudnn8-devel-ubuntu20.04.sif \
+    /bin/bash -c "source ~/.bashrc;
+        conda activate rsa;
+        pwd;
+        which python;
+        python allen_stats.py cells"
