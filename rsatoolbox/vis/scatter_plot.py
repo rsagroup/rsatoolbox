97d8155eedff337a5281c7f30275adf09dc2b094
Jasper van den Bosch <japsai@gmail.com>
2021-08-29 14:46:25 +0100

    added functions for displaying t-SNE and Isomap from #153

--- rsatoolbox/vis/scatter_plot.py
+++ rsatoolbox/vis/scatter_plot.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Optional
-import matplotlib.pyplot as plt
+from typing import TYPE_CHECKING, Literal, Optional
+import matplotlib.pyplot
+import sklearn.manifold
+import numpy
+from rsatoolbox.util.vis_utils import weight_to_matrices, Weighted_MDS
 if TYPE_CHECKING:
     from rsatoolbox.rdm import RDMs
     from numpy.typing import NDArray
     from matplotlib.figure import Figure
+seed = numpy.random.RandomState(seed=1)
 
 
 def show_scatter(
@@ -21,13 +25,13 @@ def show_scatter(
             pattern (patterns x 2)
         rdm_descriptor: (Optional[str]): If provided, this will be used as
             title for each individual RDM.
-        pattern_descriptor (Optional[str]): If provided, the chosen pattern 
+        pattern_descriptor (Optional[str]): If provided, the chosen pattern
             descriptor will be printed adjacent to each point in the plot
 
     Returns:
         Figure: A matplotlib figure in which the plot is drawn
     """
-    fig, ax = plt.subplots()
+    fig, ax = matplotlib.pyplot.subplots()
     ax.scatter(coords[0, :, 0], coords[0, :, 1])
 
     ## RDM names
@@ -37,13 +41,134 @@ def show_scatter(
     ## print labels next to dots
     if pattern_descriptor is not None:
         for p in range(coords.shape[1]):
-            ax.annotate(
+            label = ax.annotate(
                 rdms.pattern_descriptors[pattern_descriptor][p],
                 (coords[0, p, 0], coords[0, p, 1])
             )
+            label.set_alpha(.6)
 
     ## turn off all axis ticks and labels
     ax.tick_params(axis='both', which='both', bottom=False, top=False,
         right=False, left=False, labelbottom=False, labeltop=False,
         labelleft=False, labelright=False)
     return fig
+
+def show_2d(
+        rdms: RDMs,
+        method: Literal['MDS', 't-SNE', 'Isomap'],
+        weights: Optional[NDArray]=None,
+        rdm_descriptor: Optional[str]=None,
+        pattern_descriptor: Optional[str]=None
+    ) -> Figure:
+    """Draw a scatter plot of the RDMs reduced to two dimensions
+
+    Args:
+        rdms (RDMs): The RDMs object to display
+        method (str): One of 'MDS', 't-SNE', 'Isomap'.
+        weights: Optional array of weights (vector per RDM)
+        rdm_descriptor: (Optional[str]): If provided, this will be used as
+            title for each individual RDM.
+        pattern_descriptor (Optional[str]): If provided, the chosen pattern
+            descriptor will be printed adjacent to each point in the plot
+
+    Returns:
+        Figure: A matplotlib figure in which the plot is drawn
+    """
+    fitKwargs = dict()
+    if weights is not None:
+        fitKwargs['weight'] = weight_to_matrices(weights)
+    if method == 'MDS':
+        MDS = sklearn.manifold.MDS if weights is None else Weighted_MDS
+        embedding = MDS(n_components=2, random_state=seed, dissimilarity='precomputed')
+    elif method == 't-SNE':
+        embedding = sklearn.manifold.TSNE(n_components=2)
+    elif method == 'Isomap':
+        embedding = sklearn.manifold.Isomap(n_components=2)
+    else:
+        raise NotImplementedError('Unknown method: ' + str(method))
+    rdm_mats = rdms.get_matrices()
+    coords = numpy.full((rdms.n_rdm, rdms.n_cond, 2), numpy.nan)
+    for r in range(rdms.n_rdm):
+        if weights is not None:
+            coords[r, :, :] = embedding.fit_transform(rdm_mats[r, :, :], **fitKwargs)
+    return show_scatter(
+        rdms,
+        coords,
+        rdm_descriptor=rdm_descriptor,
+        pattern_descriptor=pattern_descriptor
+    )
+    
+def show_MDS(
+        rdms: RDMs,
+        weights: Optional[NDArray]=None,
+        rdm_descriptor: Optional[str]=None,
+        pattern_descriptor: Optional[str]=None
+    ) -> Figure:
+    """Draw a scatter plot based on Multidimensional Scaling dimensionality reduction
+
+    Args:
+        rdms (RDMs): The RDMs object to display
+        weights: Optional array of weights (vector per RDM)
+        rdm_descriptor: (Optional[str]): If provided, this will be used as
+            title for each individual RDM.
+        pattern_descriptor (Optional[str]): If provided, the chosen pattern
+            descriptor will be printed adjacent to each point in the plot
+
+    Returns:
+        Figure: A matplotlib figure in which the plot is drawn
+    """
+    return show_2d(
+        rdms,
+        method='MDS',
+        weights=weights,
+        rdm_descriptor=rdm_descriptor,
+        pattern_descriptor=pattern_descriptor
+    )
+
+def show_tSNE(
+        rdms: RDMs,
+        rdm_descriptor: Optional[str]=None,
+        pattern_descriptor: Optional[str]=None
+    ) -> Figure:
+    """Draw a scatter plot based on t-SNE dimensionality reduction
+
+    Args:
+        rdms (RDMs): The RDMs object to display
+        rdm_descriptor: (Optional[str]): If provided, this will be used as
+            title for each individual RDM.
+        pattern_descriptor (Optional[str]): If provided, the chosen pattern
+            descriptor will be printed adjacent to each point in the plot
+
+    Returns:
+        Figure: A matplotlib figure in which the plot is drawn
+    """
+    return show_2d(
+        rdms,
+        method='t-SNE',
+        rdm_descriptor=rdm_descriptor,
+        pattern_descriptor=pattern_descriptor
+    )
+
+def show_iso(
+        rdms: RDMs,
+        rdm_descriptor: Optional[str]=None,
+        pattern_descriptor: Optional[str]=None
+    ) -> Figure:
+    """Draw a scatter plot based on Isomap dimensionality reduction
+
+    Args:
+        rdms (RDMs): The RDMs object to display
+        rdm_descriptor: (Optional[str]): If provided, this will be used as
+            title for each individual RDM.
+        pattern_descriptor (Optional[str]): If provided, the chosen pattern
+            descriptor will be printed adjacent to each point in the plot
+
+    Returns:
+        Figure: A matplotlib figure in which the plot is drawn
+    """
+    return show_2d(
+        rdms,
+        method='Isomap',
+        rdm_descriptor=rdm_descriptor,
+        pattern_descriptor=pattern_descriptor
+    )
