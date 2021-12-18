ea525435fbd05c59e24eba8feffa0f563dfd5288
Heiko Schuett <heiko.schuett@nyu.edu>
2021-12-17 21:48:04 -0500

    style fmri_test

--- tests/test_fmri.py
+++ tests/test_fmri.py
@@ -10,6 +10,7 @@ import os
 import rsatoolbox.io.fmri as fmri
 import numpy as np
 
+
 def get_fmri_data():
     fmri_dir = os.path.join(
             os.path.dirname(__file__), "data", "BIDS_example", "derivatives",
@@ -17,11 +18,13 @@ def get_fmri_data():
     fmri_data = fmri.BidsDerivatives(fmri_dir)
     return fmri_data
 
+
 def get_subject_data():
     fmri_data = get_fmri_data()
     subjects = fmri_data.get_subjects()
     subject_data = fmri_data.subset_subject(subjects[0])                        # subset data for first subject
     return subject_data
+
 
 def get_subject_session_data():
     subject_data = get_subject_data()
@@ -29,8 +32,9 @@ def get_subject_session_data():
     subject_data_s = subject_data.subset_session_type(session_types[0])         # further subset data for first session type
     return subject_data_s
 
+
 class TestParsing(unittest.TestCase):
-    
+
     def test_full_parsing(self):
         fmri_data = get_fmri_data()
         np.testing.assert_array_equal(
@@ -41,7 +45,7 @@ class TestParsing(unittest.TestCase):
             ['ses-perceptionTest', 'ses-perceptionTraining'])
         np.testing.assert_array_equal(
             fmri_data.runs_total, 2)
-        
+
     def test_sub_parsing(self):
         subject_data = get_subject_data()
         np.testing.assert_array_equal(
@@ -56,7 +60,7 @@ class TestParsing(unittest.TestCase):
             [os.path.isdir(run_dir)
              for run_dir in subject_data.get_runs()],
             [True])
-        
+
     def test_session_parsing(self):
         subject_data = get_subject_session_data()
         np.testing.assert_array_equal(
@@ -67,10 +71,11 @@ class TestParsing(unittest.TestCase):
         np.testing.assert_array_equal(
             [os.path.isdir(run_dir)
              for run_dir in subject_data.get_runs()],
-                [True])
+            [True])
+
 
 class TestPoolingSPM(unittest.TestCase):
-    
+
     def test_beta_pooling(self):
         subject_data = get_subject_session_data()
         beta_array, beta_descriptors = subject_data.load_betas_SPM()
@@ -82,12 +87,12 @@ class TestPoolingSPM(unittest.TestCase):
         np.testing.assert_array_equal(
             np.unique(beta_descriptors),
             beta_descriptors)
-        
+
     def test_beta_pooling_w_dict(self):
         stim_ids_dict = {"Face": 1, "House": 2}
         subject_data = get_subject_session_data()
         beta_array, beta_descriptors = subject_data.load_betas_SPM(
-            stim_ids_dict = stim_ids_dict)
+            stim_ids_dict=stim_ids_dict)
         np.testing.assert_array_equal(
             beta_array.shape, (55, 60, 51, 2))
         np.testing.assert_array_equal(
@@ -96,7 +101,7 @@ class TestPoolingSPM(unittest.TestCase):
         np.testing.assert_array_equal(
             np.unique(beta_descriptors),
             beta_descriptors)
-        
+
     def test_res_pooling(self):
         subject_data = get_subject_session_data()
         res_array, res_descriptors = subject_data.load_residuals_SPM()
@@ -108,26 +113,28 @@ class TestPoolingSPM(unittest.TestCase):
         np.testing.assert_array_equal(
             np.unique(res_descriptors),
             res_descriptors)
-        
+
+
 class TestSaving(unittest.TestCase):
-    
+
     def test_saving_combo_signal(self):
         stim_ids_dict = {"Face": 1, "House": 2}
         subject_data = get_subject_session_data()
         beta_array, beta_descriptors = subject_data.load_betas_SPM(
-            stim_ids_dict = stim_ids_dict)
+            stim_ids_dict=stim_ids_dict)
         subject_data.save2combo(
-            beta_array, beta_descriptors, data_type = "signal")
+            beta_array, beta_descriptors, data_type="signal")
         assert os.path.isfile(subject_data.nifti_filename)
         assert os.path.isfile(subject_data.csv_filename)
-        
+
     def test_saving_combo_noise(self):
         subject_data = get_subject_session_data()
         res_array, res_descriptors = subject_data.load_residuals_SPM()
         subject_data.save2combo(
-            res_array, res_descriptors, data_type = "noise")
+            res_array, res_descriptors, data_type="noise")
         assert os.path.isfile(subject_data.nifti_filename)
         assert os.path.isfile(subject_data.csv_filename)
-        
+
+
 if __name__ == '__main__':
     unittest.main()
