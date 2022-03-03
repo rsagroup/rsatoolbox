9d4f800a54a306176a6bf00bc5a9c2b70ca2b7cc
Jasper van den Bosch <japsai@gmail.com>
2022-03-03 17:04:15 +0000

    bump version to 0.0.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'rsatoolbox authors'
 # The short X.Y version
 version = '0.0'
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.0.4'
 
 
 # -- General configuration ---------------------------------------------------
