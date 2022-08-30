7e15982a599cbd353546e173566527840bd454a1
Ian Charest <charest.ian@gmail.com>
2022-08-30 09:42:11 -0400

    now returns rdm objects

--- rsatoolbox/searchlight/searchlight.py
+++ rsatoolbox/searchlight/searchlight.py
@@ -1,6 +1,8 @@
 
 import warnings
 import numpy as np
+from copy import deepcopy
+from collections.abc import Iterable
 
 from joblib import Parallel, delayed, cpu_count
 from sklearn.exceptions import ConvergenceWarning
@@ -82,22 +84,22 @@ def compute_searchlight_rdms(
     prior_weight=0.1, noise=None, n_jobs=-1, verbose=0):
     """compute searchlight RDMs takes a list of indices
        and maps the betas to compute an RDM for each
-       searchlight of surface vertices. 
+       searchlight of surface vertices.
 
     Args:
-        indices (_type_): list of searchliht indices 
+        indices (_type_): list of searchliht indices
                     (see prep_surf_indices)
         betas (_type_): betas in shape n_vertices by n_conditions
         des : participant and session details
         obs_des: conditions dictionary e.g. {'conds': 'cond_0',...}
         method: metric for constructing rdm
         for a full description of the arguments, refer to calc_rdm.
-        n_jobs (int, optional): number of cpus available. 
+        n_jobs (int, optional): number of cpus available.
                     Defaults to -1 (find number of cpus automatically).
         verbose (int, optional): level of shouting. Defaults to 0.
 
     Returns:
-        array: searchlight rdms in the shape 
+        array: searchlight rdms in the shape
                n_vertices x n_pairwise_comparisons
     """
     # first deal with making datasets for the searchlights
@@ -110,13 +112,14 @@ def compute_searchlight_rdms(
                 measurements=betas[ind, :].T,
                 descriptors=des,
                 obs_descriptors=obs_des,
-                channel_descriptors=chan_des
+                channel_descriptors=chan_des,
                 )
             )
 
     # next we call calc_rdm. we use joblib parallel
-    # to distribute it if multiple cpus. this might be memory
-    # intense dependent on the number of conditions. 
+    # to distribute it if multiple cpus are present.
+    # this might be memory intense dependent on the
+    # number of conditions.
     group_iter = GroupIterator(len(data), n_jobs)
     with warnings.catch_warnings():  # might not converge
         warnings.simplefilter('ignore', ConvergenceWarning)
@@ -124,47 +127,92 @@ def compute_searchlight_rdms(
             rdms = Parallel(n_jobs=n_jobs, verbose=verbose)(
                 delayed(calc_rdm_batch)(
                     np.array(data)[list_i],
-                    method=method, 
+                    method=method,
                     descriptor='conds',
                     cv_descriptor=cv_descriptor,
                     prior_lambda=prior_lambda,
                     prior_weight=prior_weight)
                 for list_i in group_iter)
-    
-    # TODO: repack to list of RDMs object with descriptors 
-    # and chan descriptors. for now rdms is a n_vertices
-    # by n_pairwise_comparisons dissimilarities array
 
-    return np.concatenate(rdms)
+        elif isinstance(noise, np.ndarray) and noise.ndim == 2:
+            rdms = Parallel(n_jobs=n_jobs, verbose=verbose)(
+                delayed(calc_rdm_batch)(
+                    np.array(data)[list_i],
+                    method=method,
+                    descriptor='conds',
+                    noise=noise,
+                    cv_descriptor=cv_descriptor,
+                    prior_lambda=prior_lambda,
+                    prior_weight=prior_weight)
+                for list_i in group_iter)
+
+        elif isinstance(noise, Iterable):
+            rdms = Parallel(n_jobs=n_jobs, verbose=verbose)(
+                delayed(calc_rdm_batch)(
+                    np.array(data)[list_i],
+                    method=method,
+                    descriptor='conds',
+                    noise=np.array(noise)[list_i],
+                    cv_descriptor=cv_descriptor,
+                    prior_lambda=prior_lambda,
+                    prior_weight=prior_weight)
+                for list_i in group_iter)
+
+        # collect rdms in one array
+        rdms = np.concatenate(rdms)
+
+        # repack to list of RDMs object with descriptors
+        # and chan descriptors.
+        # the descriptor here becomes the index of the 
+        # centre of sphere related to the spherical indices
+        RDMs = [
+            rsr.RDMs(
+                dissimilarities=x,
+                dissimilarity_measure=method,
+                descriptors=des,
+                rdm_descriptors=deepcopy(data[y].descriptors),
+                pattern_descriptors=obs_des
+                ) for y, x in enumerate(rdms)
+            ]
+
+    return RDMs
 
 
 def calc_rdm_batch(
-    data_batch,     
-    method='correlation', descriptor=None, cv_descriptor=None, prior_lambda=1,
-    prior_weight=0.1, noise=None, n_jobs=-1, verbose=0):
+    data_batch,
+    method='correlation', descriptor='conds', cv_descriptor=None, prior_lambda=1,
+    prior_weight=0.1, noise=None):
     """ calc rdm batch
 
     Args:
-        data_batch (_type_): _description_
-        method (str, optional): _description_. Defaults to 'correlation'.
-        descriptor (_type_, optional): _description_. Defaults to None.
-        cv_descriptor (_type_, optional): _description_. Defaults to None.
-        prior_lambda (int, optional): _description_. Defaults to 1.
-        prior_weight (float, optional): _description_. Defaults to 0.1.
-        noise (_type_, optional): _description_. Defaults to None.
-        n_jobs (int, optional): _description_. Defaults to -1.
-        verbose (int, optional): _description_. Defaults to 0.
+        data_batch (list): list of rsa datasets
+        method (str, optional): metric to use. Defaults to 'correlation'.
+        descriptor (dict, optional): key in the dataset descriptors object. 
+                                     Defaults to conds.
+        noise (numpy.ndarray or list): 
+            dataset.n_channel x dataset.n_channel
+            precision matrix used to calculate the RDM
+            used only for Mahalanobis and Crossnobis estimators
+            defaults to an identity matrix, i.e. euclidean distance
+        cv_descriptor (string, optional):
+            obs_descriptor which determines the cross-validation folds.
+            Defaults to None.
+        prior_lambda (int, optional):
+            prior lambda used in symmetrized KL-divergence. Defaults to 1.
+        prior_weight (float, optional):
+            prior weight used in symmetrised KL-divergence. Defaults to 0.1.
 
     Returns:
-        _type_: _description_
+        rdms (numpy.ndarray): dissimilarities for the batch of datasets.
     """
 
     rdms = []
     for data in data_batch:
         rdm = rsr.calc_rdm(
                     data,
-                    method=method, 
+                    method=method,
                     descriptor=descriptor,
+                    noise=noise,
                     cv_descriptor=cv_descriptor,
                     prior_lambda=prior_lambda,
                     prior_weight=prior_weight)
