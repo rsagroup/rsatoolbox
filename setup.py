9cca639e285d38b7b8541fdbe57d038593bf8167
Heiko Schuett <heiko.schuett@nyu.edu>
2022-09-02 18:38:59 -0400

    a seemingly functional deployment configuration for cython

--- setup.py
+++ setup.py
@@ -1,49 +1,16 @@
-from setuptools import setup, find_packages
-from Cython.Build import cythonize
+from setuptools import setup, find_packages, Extension
+from Cython.Build import build_ext
 import numpy
-
-requires = []
-with open('requirements.txt') as reqfile:
-    requires = reqfile.read().splitlines()
 
 test_requires = []
 with open('tests/requirements.txt') as reqfile:
     test_requires = reqfile.read().splitlines()
 
-with open('README.md', encoding='utf-8') as readmefile:
-    long_description = readmefile.read()
-
-
 setup(
-    name='rsatoolbox',
-    version='0.0.4',
-    description='Representational Similarity Analysis (RSA) in Python',
-    url='https://github.com/rsagroup/rsatoolbox',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    classifiers=[
-        'Programming Language :: Python',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Development Status :: 4 - Beta',
-        'Topic :: Scientific/Engineering',
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
-    author='rsatoolbox authors',
-    author_email='',
-    keywords='neuroscience ',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=requires,
     tests_require=test_requires,
     test_suite='tests',
-    ext_modules=cythonize(
-        'rsatoolbox/cutil/*.pyx',
-        #include_path = [numpy.get_include()],
-        language_level='3'
-    )
+    url = "https://github.com/rsagroup/rsatoolbox",
+    zip_safe = False,
+    extensions = [Extension("*", ["*.pyx"])],
+    cmdclass={'build_ext': build_ext}
 )
