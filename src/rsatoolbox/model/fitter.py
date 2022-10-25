92291b3311893357714bc2baa507c8b5f7f011bd
Jasper van den Bosch <someone@somewhere.com>
2022-10-25 16:36:48 +0100

    refactored _parse_input_rdms for clarity

--- src/rsatoolbox/model/fitter.py
+++ src/rsatoolbox/model/fitter.py
@@ -10,7 +10,7 @@ import scipy.sparse
 from rsatoolbox.rdm import compare
 from rsatoolbox.util.matrix import get_v
 from rsatoolbox.util.pooling import pool_rdm
-from rsatoolbox.util.rdm_utils import _parse_input_rdms
+from rsatoolbox.util.rdm_utils import _parse_nan_vectors
 
 
 class Fitter:
@@ -264,7 +264,7 @@ def fit_regress(model, data, method='cosine', pattern_idx=None,
     vectors = pred.get_vectors()
     data_mean = pool_rdm(data, method=method)
     y = data_mean.get_vectors()
-    vectors, y, nan_idx = _parse_input_rdms(vectors, y)
+    vectors, y, nan_idx = _parse_nan_vectors(vectors, y)
     # Normalizations
     if method == 'cosine':
         v = None
@@ -335,7 +335,7 @@ def fit_regress_nn(model, data, method='cosine', pattern_idx=None,
     vectors = pred.get_vectors()
     data_mean = pool_rdm(data, method=method)
     y = data_mean.get_vectors()
-    vectors, y, nan_idx = _parse_input_rdms(vectors, y)
+    vectors, y, non_nan_mask = _parse_nan_vectors(vectors, y)
     # Normalizations
     if method == 'cosine':
         v = None
@@ -344,12 +344,12 @@ def fit_regress_nn(model, data, method='cosine', pattern_idx=None,
         v = None
     elif method == 'cosine_cov':
         v = get_v(pred.n_cond, sigma_k)
-        v = v[nan_idx[0]][:, nan_idx[0]]
+        v = v[non_nan_mask[0]][:, non_nan_mask[0]]
     elif method == 'corr_cov':
         vectors = vectors - np.mean(vectors, 1, keepdims=True)
         y = y - np.mean(y)
         v = get_v(pred.n_cond, sigma_k)
-        v = v[nan_idx[0]][:, nan_idx[0]]
+        v = v[non_nan_mask[0]][:, non_nan_mask[0]]
     else:
         raise ValueError('method argument invalid')
     theta, _ = _nn_least_squares(vectors.T, y[0], ridge_weight=ridge_weight, V=v)
