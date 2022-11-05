8f271094ab9db1fb08b2d08cbd0f44ab96148314
Jasper van den Bosch <someone@somewhere.com>
2022-11-05 09:41:37 +0100

    further consolidated build options in pyproject.toml

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
+"""Setup.py now only remains as a build script for the cython extensions.
+Using setup.py for other things is now deprecated and may have unexpected results: 
+    setup.py test -> pytest
+    setup.py develop -> pip install -e
+"""
 from setuptools import setup, Extension
 import setuptools_scm  # noqa: F401
 from Cython.Build import build_ext ## missing dev time req
-from os.path import isfile
 
-test_requires = []
-if isfile('tests/requirements.txt'):
-    with open('tests/requirements.txt') as reqfile:
-        test_requires = reqfile.read().splitlines()
 
 setup(
-    tests_require=test_requires,
-    test_suite='tests',
-    url = "https://github.com/rsagroup/rsatoolbox",
-    zip_safe = False,
     ext_modules = [
         Extension(
             "rsatoolbox.cengine.similarity",
