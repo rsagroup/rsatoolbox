a3e2c173590b089d810126246b469114caf91fe9
Jasper van den Bosch <someone@somewhere.com>
2022-12-04 14:24:42 +0000

    alabaster theme option to display version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,7 +72,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -95,7 +95,7 @@ html_theme = 'alabaster'
 # documentation.
 #
 html_theme_options = dict(
-    display_version=True
+    extra_nav_links={f'version: {release}': ''}
 )
 
 # Add any paths that contain custom static files (such as style sheets) here,
