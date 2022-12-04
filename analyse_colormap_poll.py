0d6a5689cae0f48176c07b9fb5a5e2d1fed134ba
Jasper van den Bosch <someone@somewhere.com>
2022-12-04 13:43:24 +0000

    using new poll version

--- analyse_colormap_poll.py
+++ analyse_colormap_poll.py
@@ -3,7 +3,7 @@
 from os.path import expanduser, join
 import pandas
 
-fname = 'Meadows_rsatoolbox_color_maps_v_v2_annotations.csv'
+fname = 'Meadows_rsatoolbox_color_maps_v_v3_annotations.csv'
 fpath = join(expanduser('~/data/rsatoolbox/colormaps/'), fname)
 df = pandas.read_csv(fpath)
 df['nmoves'] = [int(l.split('_')[0]) for l in df.label.values]
@@ -14,14 +14,15 @@ for _, row in df[df.nmoves > 0].iterrows():
         votes[cmap].append(rank)
 print(sorted(votes.items(), key=lambda cmap_tuple: sum(cmap_tuple[1])))
 
-"""
-Out[20]:
-[('inferno', [0, 1]),
- ('magma', [2, 0]),
- ('current', [3, 2]),
- ('plasma', [1, 7]),
- ('flare_r', [4, 5]),
- ('cividis', [7, 3]),
- ('crest_r', [6, 4]),
- ('viridis', [5, 6])]
+""" 3 votes:
+[('bone', [1, 0, 0]),
+ ('gray', [0, 1, 1]),
+ ('inferno', [6, 3, 3]),
+ ('cividis', [3, 5, 5]),
+ ('magma', [7, 4, 2]),
+ ('crest_r', [2, 6, 8]),
+ ('current', [5, 2, 9]),
+ ('viridis', [4, 9, 4]),
+ ('plasma', [8, 8, 6]),
+ ('flare_r', [9, 7, 7])]
 """
