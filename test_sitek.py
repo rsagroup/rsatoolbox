b9cdbd6da4a51b6bf1addbf678d59abf8188260f
Jasper van den Bosch <someone@somewhere.com>
2023-02-20 17:04:50 +0000

    different masking strategy for sitek data

--- test_sitek.py
+++ test_sitek.py
@@ -11,7 +11,7 @@ import nibabel as nib
 from rsatoolbox.rdm.rdms import RDMs
 from rsatoolbox.model.model import ModelFixed
 from rsatoolbox.inference import eval_fixed
-from rsatoolbox.util.searchlight import (
+from rsatoolbox.searchlight.volume import (
     get_volume_searchlight, get_searchlight_RDMs, evaluate_models_searchlight)
 
 data_dir = expanduser('~/data/rsatoolbox/248b_sitek')
@@ -21,23 +21,29 @@ image_paths = glob(join(data_dir, '*beta.nii.gz'))
 
 mask_img = nib.load(mask_fpath)
 mask_data = mask_img.get_fdata()
-mask = ~np.isnan(mask_data) # daniel
 
-## 5mins
-centers, neighbors = get_volume_searchlight(mask, radius=5, threshold=0.5)
 
 # loop over all images
 x, y, z = mask_data.shape
 print('reserving memory for betas..')
 data = np.zeros((len(image_paths), x, y, z))
-for x, im in enumerate(image_paths):
-    print(f'loading image {x+1}/{len(image_paths)}')
-    data[x] = nib.load(im).get_fdata()
+for i, im in enumerate(image_paths):
+    print(f'loading image {i+1}/{len(image_paths)}')
+    data[i] = nib.load(im).get_fdata()
 
 # reshape data so we have n_observastions x n_voxels
 data_2d = data.reshape([data.shape[0], -1])
 data_2d = np.nan_to_num(data_2d)
-# Get RDMs (13m30)
+
+
+## mask based on all-zero patterns
+mask_2d = ~np.all(data_2d==0, axis=0)
+mask_3d = mask_2d.reshape(x, y, z)
+
+## 5mins
+centers, neighbors = get_volume_searchlight(mask_3d, radius=5, threshold=0.5)
+
+# Get RDMs
 # only one pattern per image
 events = np.arange(len(image_paths))
 SL_RDM = get_searchlight_RDMs(data_2d, centers, neighbors, events, method='correlation')
@@ -89,5 +95,6 @@ model_rdms = RDMs(
     dissimilarity_measure='Euclidean'
 )
 tone_model = ModelFixed( 'Tone RDM', model_rdms.subset('categorical_model', 'tone'))
+
 
 evaluate_models_searchlight(SL_RDM, tone_model, eval_fixed, method='spearman', n_jobs=4)
