b9f9bdba54ff328683dec2f4e95a09f710bee9d4
Jasper van den Bosch <japsai@gmail.com>
2023-05-30 15:03:09 +0100

    removed existing test for merge_subsets now part of merge_datasets

--- tests/test_data.py
+++ tests/test_data.py
@@ -588,37 +588,6 @@ class TestSave(unittest.TestCase):
         assert data_loaded.descriptors['subj'] == 0
 
 
-class TestMerge(unittest.TestCase):
-
-    def setUp(self):
-        self.rng = np.random.default_rng(0)
-        measurements = self.rng.random((4, 10))
-        des = {'session': 0, 'subj': 0}
-        obs_des = {'conds': np.array([str(i) for i in range(1, 5)])}
-        chn_des = {'rois': np.array([chr(l) for l in range(65, 75)])}
-        self.test_data = rsd.Dataset(
-            measurements=measurements,
-            descriptors=des,
-            obs_descriptors=obs_des,
-            channel_descriptors=chn_des
-        )
-
-    def test_merge(self):
-        subsets = self.test_data.split_obs('conds')
-        self.test_data_merged = rsd.merge_subsets(subsets)
-        np.testing.assert_array_equal(
-            self.test_data_merged.measurements,
-            self.test_data.measurements)
-        self.assertEqual(self.test_data_merged.descriptors,
-                         self.test_data.descriptors)
-        np.testing.assert_array_equal(
-            self.test_data_merged.obs_descriptors['conds'],
-            self.test_data.obs_descriptors['conds'])
-        np.testing.assert_array_equal(
-            self.test_data_merged.channel_descriptors['rois'],
-            self.test_data.channel_descriptors['rois'])
-
-
 class TestOESplit(unittest.TestCase):
 
     def setUp(self) -> None:
