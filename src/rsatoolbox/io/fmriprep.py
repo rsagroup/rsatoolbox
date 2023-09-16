fe0b8a3618f7d35f7c95cd2bb66d73c4dfd86d05
Jasper van den Bosch <japsai@gmail.com>
2023-09-16 22:13:47 +0100

    simple standard hrf

--- src/rsatoolbox/io/fmriprep.py
+++ src/rsatoolbox/io/fmriprep.py
@@ -3,9 +3,14 @@
 from __future__ import annotations
 from os.path import relpath, join
 from typing import TYPE_CHECKING, List, Dict, Optional
+import numpy
+from scipy.interpolate import pchip
 from rsatoolbox.io.bids import BidsLayout
+from rsatoolbox.io.hrf import HRF
 if TYPE_CHECKING:
     from rsatoolbox.io.bids import BidsMriFile
+    from pandas import DataFrame
+    from numpy.typing import NDArray
 
 
 def find_fmriprep_runs(
@@ -68,3 +73,43 @@ class FmriprepRun:
         fmriprep_prefix = join('derivatives', 'fmriprep')
         fp_path = relpath(self.boldFile.relpath, fmriprep_prefix)
         return f'<{self.__class__.__name__} [{fp_path}]>'
+
+
+def make_design_matrix(events: DataFrame, tr: float, n_vols: int) -> NDArray:
+    """Create a matrix of HRF-convolved predictors from BIDS events
+
+    Args:
+        events (DataFrame): BIDS-style table of events
+        tr (float): Time to repeat scan in seconds
+
+    Returns:
+        NDArray: volumes * conditions
+    """
+    block_dur = numpy.median(events.duration)
+
+    ## convolve a standard HRF to the block shape in the design
+    STANDARD_TR = 0.1
+    hrf = numpy.convolve(HRF, numpy.ones(int(block_dur/STANDARD_TR)))
+
+    ## timepoints in block (32x)
+    timepts_block = numpy.arange(0, int((hrf.size-1)*STANDARD_TR), tr)
+
+    # resample to desired TR
+    hrf = pchip(numpy.arange(hrf.size)*STANDARD_TR, hrf)(timepts_block)
+    hrf = hrf / hrf.max()
+
+    ## make design matrix
+    conditions = events.trial_type.unique()
+    dm = numpy.zeros((n_vols, conditions.size))
+    all_times = numpy.linspace(0, tr*(n_vols-1), n_vols)
+    hrf_times = numpy.linspace(0, tr*(len(hrf)-1), len(hrf))
+    for c, condition in enumerate(conditions):
+        onsets = events[events.trial_type == condition].onset.values
+        yvals = numpy.zeros((n_vols))
+        # loop over blocks
+        for o in onsets:
+            # interpolate to find values at the data sampling time points
+            f = pchip(o + hrf_times, hrf, extrapolate=False)(all_times)
+            yvals = yvals + numpy.nan_to_num(f)
+        dm[:, c] = yvals
+    return dm
