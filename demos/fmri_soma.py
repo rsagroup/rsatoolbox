4093d28bd72385b7ed0a39abbc57e529daca5f99
Jasper van den Bosch <japsai@gmail.com>
2023-09-16 22:14:39 +0100

    glm

--- demos/fmri_soma.py
+++ demos/fmri_soma.py
@@ -46,62 +46,6 @@ bold_fpath = join(
 )
 bold_data = nibabel.load(bold_fpath).get_fdata()
 
-## make mask
-mean_bold = bold_data.mean(axis=3)
-mask = mean_bold>mean_bold.mean()*1.5
-data = bold_data[mask, :]
-#plt.imshow(mean_bold[:,:, 30])
-
-
-## make design matrix
-n_vols = bold_data.shape[-1]
-block_dur = numpy.median(events_df.duration)
-
-## convolve a standard HRF to the block shape in the design
-STANDARD_HRF = numpy.load('demos/hrf.npy')
-STANDARD_TR = 0.1
-hrf = numpy.convolve(STANDARD_HRF, numpy.ones(int(block_dur/STANDARD_TR)))
-
-## timepoints in block (32x)
-timepts_block = numpy.arange(0, int((hrf.size-1)*STANDARD_TR), tr)
-
-# resample to desired TR
-hrf = pchip(numpy.arange(hrf.size)*STANDARD_TR, hrf)(timepts_block)
-hrf = hrf / hrf.max()
-
-## make design matrix
-conditions = events_df.trial_type.unique()
-dm = numpy.zeros((n_vols, conditions.size))
-all_times = numpy.linspace(0, tr*(n_vols-1), n_vols)
-hrf_times = numpy.linspace(0, tr*(len(hrf)-1), len(hrf))
-for c, condition in enumerate(conditions):
-    onsets = events_df[events_df.trial_type == condition].onset.values
-    yvals = numpy.zeros((n_vols))
-    # loop over blocks
-    for o in onsets:
-        # interpolate to find values at the data sampling time points
-        f = pchip(o + hrf_times, hrf, extrapolate=False)(all_times)
-        yvals = yvals + numpy.nan_to_num(f)
-    dm[:, c] = yvals
-
-## add polynomials
-pdata = wdata / wdata.mean(axis=0)
-
-## least square fitting
-# The matrix addition is equivalent to concatenating the list of data and the list of
-# design and fit it all at once. However, this is more memory efficient.
-design = [dm]
-data = [data.T]
-X = numpy.vstack(design)
-X = numpy.linalg.inv(X.T @ X) @ X.T
-
-betas = 0
-start_col = 0
-for run in range(len(data)):
-    n_vols = data[run].shape[0]
-    these_cols = numpy.arange(n_vols) + start_col
-    betas += X[:, these_cols] @ data[run]
-    start_col += data[run].shape[0]
 
 ## i'm guessing this. baseline + conditions mentioned in readme
 labels = ['baseline', 'toe', 'ankle', 'left leg', 'right leg', 'finger', 'wrist', 
