7a9fc9164a1a9132d9ddd01d13c940e404371810
Jörn Diedrichsen <joern.diedrichsen@googlemail.com>
2024-06-16 21:49:59 -0400

    SPM example

--- src/rsatoolbox/io/spm.py
+++ src/rsatoolbox/io/spm.py
@@ -1,95 +1,100 @@
-"""Loading SPM (Statistical Parametric Mapping) fMRI data
+"""Handeling SPM (Statistical Parametric Mapping) fMRI data
 
-SPM stores GLM results as one volume per condition. This module has functions to
-load these beta images or their residuals as a pooled data object. This can then be stored
-in a new format.
+Utility object that helps to extract time series data, beta coefficients, and residuals from a GLM stored in a SPM.mat file.
 
 ## Usage
 ```
-betas = rsatoolbox.io.spm.load_betas('/dir/imgs/')
-betas.save2combo() ## stores /dir/imgs.nii.gz and /dir/imgs.csv
-betas.to_dataset() ## not implemented yet
+spm = SpmGlm('path/to/spm')
+spm.get_info_from_spm_mat()
+[residuals, beta, info] = spm.get_residuals('my_ROI_Mask.nii')
 ```
 """
 from __future__ import annotations
 from typing import TYPE_CHECKING, List, Optional, Callable
 from os.path import join, normpath
-import glob
-from rsatoolbox.io.optional import import_nibabel
+import nibabel as nb
+import numpy as np
+import nitools as nt
 from pandas import DataFrame
 from numpy import stack
+from scipy.io import loadmat
+
 if TYPE_CHECKING:
     from numpy.typing import NDArray
 
-
-def load_betas(path: str, stim_ids_dict=None) -> SpmGlm:
-    return SpmGlm(path).load_betas(stim_ids_dict)
-
-
-def load_residuals(path: str, res_range=None) -> SpmGlm:
-    return SpmGlm(path).load_residuals(res_range)
-
-
 class SpmGlm:
-    """SPM glm 
+    """class for handling first-levels GLMs estimated in SPM
 
     Attributes:
         path (str):
-            paths to directory containing beta NIfTi files
-        pooled_data_array (4d numpy array):
-            all beta 3d arrays for the keys in stim_ids_dict found in each
-            run directory stacked along the fourth dimension
-        dim4_descriptors (list of str):
-            corresponding descriptors
-            e.g. ['cond_face_run_05', 'cond_house_run_30']
+            paths to directory containing SPM files
     """
-
-    path: str
-    pooled_data_array: NDArray ## x * y * z * conds
-    dim4_descriptors: List[str]
-    affine: NDArray
-    glob: Callable[[str], List[str]]
 
     def __init__(self, path: str, nibabelMock=None, globMock=None):
         self.path = normpath(path)
-        self.nibabel = import_nibabel(nibabelMock)
-        self.glob = globMock or glob.glob
+        # self.nibabel = import_nibabel(nibabelMock)
+        # self.glob = globMock or glob.glob
 
-    def load_betas(self, stim_ids_dict=None):
-        """
-        Collects 3d images of prespecified beta coefficients
-        (typical SPM GLM results) and corresponding metadata
-        (condition + run info) into respective lists
+    def get_info_from_spm_mat(self):
+        """Initializes information for SPM.mat file
 
         Args:
-            stim_ids_dict (dict): {condition : beta coefficient number}
-                e.g. {'face': 1, 'house': 2}
+            spm_mat_path (str): _description_
+        """
+        SPM = loadmat(f"{self.path}/SPM.mat", simplify_cells=True)['SPM']
+        # Get basic information from SPM.mat
+        self.nscans = SPM['nscan']
+        self.nruns = len(self.nscans)
+        # Get the name and information on all the beta files``
+        self.beta_files = [v['fname'] for v in SPM['Vbeta']]
+        self.beta_names = []
+        self.run_number = []
+        # Extract run number and condition name from SPM names
+        for reg_name in SPM['xX']['name']:
+            s=reg_name.split(' ')
+            self.run_number.append(int(s[0][3:-1]))
+            self.beta_names.append(s[1])
+        self.run_number = np.array(self.run_number)
+        self.beta_names = np.array(self.beta_names)
+        # Get the raw data file name
+        self.rawdata_files = SPM['xY']['P']
+        # Get the necesssary matrices to reestimate the GLM for getting the residuals
+        self.filter_matrices = [k['X0'] for k in SPM['xX']['K']]
+        self.reg_of_interest = SPM['xX']['iC']
+        self.design_matrix = SPM['xX']['xKXs']['X'] # Filtered and whitened design matrix
+        self.eff_df = SPM['xX']['erdf'] # Effective degrees of freedom
+        self.weight = SPM['xX']['W'] # Weight matrix for whitening
+        self.pinvX = SPM['xX']['pKX'] # Pseudo-inverse of (filtered and weighted) design matrix
+
+    def get_betas(self,mask):
+        """
+        Samples the beta images of an estimated SPM GLM at the mask locations
+        also returns the ResMS values, and the obseration descriptors (run and condition) name
+
+        Args:
+            mask (ndarray or nibabel nifti1image):
+                Indicates which voxels to extract
+                Could be a binary 3d-array or a nifti image of the same size as the data
+                Or a 3xP array of coordinates to extract (in mm space)
+        Returns:
+            data (ndarray): N x P array of beta coefficients
+            resms (ndarray): 1d array of ResMS values
+            obs_descriptors (dict): with lists reg_name and run_number (N long)
         """
 
-        if stim_ids_dict is None:
-            n_conds = len(self.glob(join(self.path, "beta*")))
-            keys = [str(cond_num).zfill(4) for cond_num in range(1, n_conds+1)]
-            values = [cond_num for cond_num in range(1, n_conds+1)]
-            stim_ids_dict = dict(zip(keys, values))
+        coords = nt.get_mask_coords(mask)
 
-        beta_array_superset = []
-        self.dim4_descriptors = []
-        run_counter = 0
+        # Generate the list of relevant beta images:
+        indx = self.reg_of_interest-1
+        beta_files = [f'{self.path}/{self.beta_files[i]}' for i in indx]
+        # Get the data from beta and ResMS files
+        rms_file = [f'{self.path}/ResMS.nii']
+        data = nt.sample_images(beta_files + rms_file,coords,use_dataobj=False)
+        # Return the data and the observation descriptors
+        info = {'reg_name': self.beta_names[indx], 'run_number': self.run_number[indx]}
+        return data[:-1,:], data[-1,:], info
 
-        run_counter += 1
-        for condition in stim_ids_dict.keys():
-            num = stim_ids_dict[condition]
-            beta_image_path = join(self.path, "beta_" + str(num).zfill(4))
-            beta_image = self.nibabel.nifti1.load(beta_image_path)
-            beta_array_superset.append(beta_image.get_fdata())
-            self.dim4_descriptors.append("cond_" + condition + "_run_" +
-                                    str(run_counter).zfill(2))
-        # Get affine matrix
-        self.affine = beta_image.affine.copy()
-        self.pooled_data_array = stack(beta_array_superset, axis=3)
-        return self
-
-    def load_residuals(self, res_range=None):
+    def get_residuals(self,mask):
         """
         Collects 3d images of a range of GLM residuals
         (typical SPM GLM results) and corresponding metadata
@@ -98,67 +103,37 @@ class SpmGlm:
         Args:
             res_range (range): range of to be saved residual images per run
         """
+        # Sample the relevant time series data
+        coords = nt.get_mask_coords(mask)
+        data = nt.sample_images(self.rawdata_files,coords,use_dataobj=True)
 
-        residual_array_superset = []
-        self.dim4_descriptors = []
-        self.n_res = len(self.glob(join(self.path, "Res_*")))
-        if isinstance(res_range, range):
-            assert self.n_res >= max(res_range), \
-                "res_range outside of existing residuals"
-        else:
-            res_range = range(1, self.n_res+1)
+        # Filter and temporal pre-whiten the data
+        fdata= self.spm_filter(self.weight @ data) # spm_filter
 
-        run_counter = 0
+        # Estimate the beta coefficients abd residuals
+        beta = self.pinvX @ fdata
+        residuals = fdata - self.design_matrix @ beta
 
-        run_counter += 1
-        for res in res_range:
-            res_image_path = join(self.path, "Res_" +
-                                            str(res).zfill(4))
-            res_image = self.nibabel.nifti1.load(res_image_path)
-            residual_array_superset.append(res_image.get_fdata())
-            self.dim4_descriptors.append("res_" + str(res).zfill(4) + "_run_" +
-                                    str(run_counter).zfill(2))
-        # Get affine matrix
-        self.subject_affine = res_image.affine.copy()
-        self.pooled_data_array = stack(residual_array_superset, axis=3)
-        return self
+        # Return the regressors of interest
+        indx = self.reg_of_interest-1
+        info = {'reg_name': self.beta_names[indx], 'run_number': self.run_number[indx]}
+        return residuals, beta[indx,:], info
 
-    def save2nifti(self, fpath: Optional[str]=None):
+    def spm_filter(self,data):
         """
-        Converts 4d array to subject-specific 4d NIfTi image
-        of beta coeffients or residuals and saves it to your OS
+        Does high pass-filtering and temporal weighting of the data (indentical to spm_filter)
 
         Args:
-            fpath (str):
-                path to which you want to save your data
+            data (ndarray): 2d array of time series data (TxP)
+        Returns:
+            data (ndarray): 2d array of time series data (TxP)
         """
-        fpath = fpath or (self.path + '.nii.gz')
-        pooled_data = self.nibabel.nifti1.Nifti1Image(
-            self.pooled_data_array,
-            self.affine
-        )
-        self.nifti_filename = join(fpath)
-        self.nibabel.nifti1.save(pooled_data, self.nifti_filename)
+        scan_bounds = self.nscans.cumsum()
+        scan_bounds = np.insert(scan_bounds,0,0)
 
-    def save2csv(self, fpath: Optional[str]=None):
-        """
-        Saves subject-specific 4d NIfTi image descriptors to a csv file
+        fdata = data.copy()
+        for i in range(self.nruns):
+            Y = fdata[scan_bounds[i]:scan_bounds[i+1],:];
+            Y = Y - self.filter_matrices[i] @ (self.filter_matrices[i].T @ Y)
+        return fdata
 
-        Args:
-            descriptors (list of str):
-                descriptors of fourth a NIfTi file's 4th dimension
-            fpath (str):
-                path to which you want to save your data
-        """
-        fpath = fpath or (self.path + '.csv')
-        df = DataFrame({'descriptor': self.dim4_descriptors})
-        df.to_csv(fpath, header=False)
-
-    def save2combo(self, fpath: Optional[str]=None):
-        """
-        Combined saving of fmri data and descriptors
-        """
-        fpath_niigz = fpath or (self.path + '.nii.gz')
-        fpath_csv = fpath_niigz.replace('.nii.gz', '.csv')
-        self.save2nifti(fpath_niigz)
-        self.save2csv(fpath_csv)
