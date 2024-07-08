a7eb68bf88c9fc4ebcc3b5c6929367f56829fd70
Jasper van den Bosch <japsai@gmail.com>
2024-07-08 23:37:52 +0100

    style fixes in fmriprep

--- src/rsatoolbox/io/fmriprep.py
+++ src/rsatoolbox/io/fmriprep.py
@@ -15,7 +15,7 @@ if TYPE_CHECKING:
 
 def find_fmriprep_runs(
         bids_root_path: str,
-        tasks: Optional[List[str]]=None) -> List[FmriprepRun]:
+        tasks: Optional[List[str]] = None) -> List[FmriprepRun]:
     """Find all sub/ses/task/run entries for which there is a preproc_bold file
     """
     bids = BidsLayout(bids_root_path)
@@ -44,7 +44,7 @@ class FmriprepRun:
     def __init__(self, boldFile: BidsMriFile) -> None:
         self.boldFile = boldFile
 
-    def get_data(self, masked: bool=False):
+    def get_data(self, masked: bool = False):
         data = self.boldFile.get_data()
         if masked:
             return data[self.get_mask(), :]
@@ -60,29 +60,38 @@ class FmriprepRun:
     def get_mask(self) -> NDArray:
         mask_file = self.boldFile.get_mri_sibling(desc='brain', suffix='mask')
         return mask_file.get_data().astype(bool)
-    
-    def get_confounds(self, cf_names: Optional[List[str]]=None) -> DataFrame:
+
+    def get_confounds(self, cf_names: Optional[List[str]] = None) -> DataFrame:
         """_summary_
 
         Returns:
             DataFrame: _description_
         """
         cf_names = cf_names or ['global_signal', 'csf', 'white_matter',
-            'trans_x', 'trans_y', 'trans_z', 'rot_x', 'rot_y', 'rot_z']
-        confounds_file = self.boldFile.get_table_sibling(desc='confounds', suffix='timeseries')
+                                'trans_x', 'trans_y', 'trans_z', 'rot_x',
+                                'rot_y', 'rot_z']
+        confounds_file = self.boldFile.get_table_sibling(
+            desc='confounds', suffix='timeseries')
         df = confounds_file.get_frame()
         return df[cf_names]
-    
+
     def get_parcellation(self):
-        parc_file = self.boldFile.get_mri_sibling(desc='aparcaseg', suffix='dseg')
+        parc_file = self.boldFile.get_mri_sibling(
+            desc='aparcaseg', suffix='dseg')
         return parc_file.get_data().astype(int)
-    
+
     def get_parcellation_labels(self):
-        parc_file = self.boldFile.get_mri_sibling(desc='aparcaseg', suffix='dseg')
+        parc_file = self.boldFile.get_mri_sibling(
+            desc='aparcaseg', suffix='dseg')
         return parc_file.get_key().get_frame().set_index('index')
-    
-    def to_descriptors(self, collapse_by_trial_type: bool=False, masked: bool=False) -> Dict:
-        """Get dictionary of dataset, observation and channel- level descriptors
+
+    def to_descriptors(
+        self,
+        collapse_by_trial_type: bool = False,
+        masked: bool = False
+    ) -> Dict:
+        """Get dictionary of dataset, observation and
+        channel- level descriptors
 
         Returns:
             Dict: kwargs for DatasetBase with keys:
@@ -106,16 +115,19 @@ class FmriprepRun:
         if self.boldFile.run:
             ds_descs['task'] = self.boldFile.task
         return ds_descs
-    
-    def get_obs_descriptors(self, collapse_by_trial_type: bool=False) -> Dict:
+
+    def get_obs_descriptors(
+            self, collapse_by_trial_type: bool = False
+    ) -> Dict:
         obs_descs = dict()
+        trial_type = self.boldFile.get_events()['trial_type']
         if collapse_by_trial_type:
-            obs_descs['trial_type'] = self.boldFile.get_events()['trial_type'].unique()
+            obs_descs['trial_type'] = trial_type.unique()
         else:
-            obs_descs['trial_type'] = self.boldFile.get_events()['trial_type'].values
+            obs_descs['trial_type'] = trial_type.values
         return obs_descs
-    
-    def get_channel_descriptors(self, masked: bool=False) -> Dict:
+
+    def get_channel_descriptors(self, masked: bool = False) -> Dict:
         parc_ix_3d = self.get_parcellation()
         if masked:
             parc_ix = parc_ix_3d[self.get_mask()]
@@ -124,15 +136,19 @@ class FmriprepRun:
         labels_df = self.get_parcellation_labels()
         labels = labels_df.loc[parc_ix]['name'].values
         return dict(aparcaseg=labels)
-    
+
     def __repr__(self) -> str:
         fmriprep_prefix = join('derivatives', 'fmriprep')
         fp_path = relpath(self.boldFile.relpath, fmriprep_prefix)
         return f'<{self.__class__.__name__} [{fp_path}]>'
 
 
-def make_design_matrix(events: DataFrame, tr: float, n_vols: int,
-                       confounds: Optional[DataFrame]) -> Tuple[NDArray, NDArray, int]:
+def make_design_matrix(
+        events: DataFrame,
+        tr: float,
+        n_vols: int,
+        confounds: Optional[DataFrame]
+) -> Tuple[NDArray, NDArray, int]:
     """Create a matrix of HRF-convolved predictors from BIDS events
 
     Args:
@@ -149,18 +165,18 @@ def make_design_matrix(events: DataFrame, tr: float, n_vols: int,
     """
     block_dur = numpy.median(events.duration)
 
-    ## convolve a standard HRF to the block shape in the design
+    # convolve a standard HRF to the block shape in the design
     STANDARD_TR = 0.1
     hrf = numpy.convolve(HRF, numpy.ones(int(block_dur/STANDARD_TR)))
 
-    ## timepoints in block (32x)
+    # timepoints in block (32x)
     timepts_block = numpy.arange(0, int((hrf.size-1)*STANDARD_TR), tr)
 
     # resample to desired TR
     hrf = pchip(numpy.arange(hrf.size)*STANDARD_TR, hrf)(timepts_block)
     hrf = hrf / hrf.max()
 
-    ## make design matrix
+    # make design matrix
     conditions = events.trial_type.unique()
     dm = numpy.zeros((n_vols, conditions.size))
     all_times = numpy.linspace(0, tr*(n_vols-1), n_vols)
@@ -178,12 +194,12 @@ def make_design_matrix(events: DataFrame, tr: float, n_vols: int,
 
     if confounds is not None:
         assert confounds.shape[0] == n_vols
-        ## derivatives have n/a values for first vol
+        # derivatives have n/a values for first vol
         cf = confounds.dropna(axis=1).values
-        #cf = (cf - cf.mean(axis=0)) / (cf.max(axis=0) - cf.min(axis=0))
+        # cf = (cf - cf.mean(axis=0)) / (cf.max(axis=0) - cf.min(axis=0))
         dm = numpy.hstack([dm, cf])
         pred_mask = numpy.hstack([pred_mask, numpy.zeros(cf.shape[1])])
     dm = (dm - dm.mean(axis=0)) / (dm.max(axis=0) - dm.min(axis=0))
     dof = n_vols - dm.shape[1]
-    
+
     return dm, pred_mask.astype(bool), dof
