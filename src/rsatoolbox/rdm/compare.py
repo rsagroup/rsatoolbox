Experimenting with using all-ones and all-zeros with the whitened cosine comparator, after correcting the distance covariance matrix.

src/rsatoolbox/rdm/compare.py
@@ -3,12 +3,14 @@
 """
 Comparison methods for comparing two RDMs objects
 """
+import itertools as it
 import numpy as np
 import scipy.stats
 from scipy import linalg
 from scipy.optimize import minimize
 from scipy.stats._stats import _kendall_dis
 from scipy.spatial.distance import squareform
+from statistics import mode
 from rsatoolbox.util.matrix import pairwise_contrast_sparse
 from rsatoolbox.util.matrix import pairwise_contrast
 from rsatoolbox.util.rdm_utils import _get_n_from_reduced_vectors
@@ -132,7 +134,9 @@ def compare_cosine_cov_weighted(rdm1, rdm2, sigma_k=None):
 
     """
     vector1, vector2, nan_idx = _parse_input_rdms(rdm1, rdm2)
-    sim = _cosine_cov_weighted(vector1, vector2, sigma_k, nan_idx)
+    stim_nums = np.array(rdm1.pattern_descriptors['stimulus'])
+    frozen_inds = list(np.where(stim_nums == -1)[0]) + list(np.where(stim_nums == -2)[0])
+    sim = _cosine_cov_weighted(vector1, vector2, frozen_inds, sigma_k, nan_idx)
     return sim
 
 
@@ -259,16 +263,16 @@ def compare_neg_riemannian_distance(rdm1, rdm2, sigma_k=None):
     n_cond = _get_n_from_length(vector1.shape[1])
     if sigma_k is None:
         sigma_k = np.eye(n_cond)
-    P = np.block([-1*np.ones((n_cond - 1, 1)), np.eye(n_cond - 1)])
-    sigma_k_hat = P@sigma_k@P.T
+    P = np.block([-1 * np.ones((n_cond - 1, 1)), np.eye(n_cond - 1)])
+    sigma_k_hat = P @ sigma_k @ P.T
     # construct RDM to 2nd-moment (G) transformation
-    pairs = pairwise_contrast(np.arange(n_cond-1))
+    pairs = pairwise_contrast(np.arange(n_cond - 1))
     pairs[pairs == -1] = 1
     T = np.block([
-        [np.eye(n_cond - 1), np.zeros((n_cond-1, vector1.shape[1] - n_cond + 1))],
+        [np.eye(n_cond - 1), np.zeros((n_cond - 1, vector1.shape[1] - n_cond + 1))],
         [0.5 * pairs, np.diag(-0.5 * np.ones(vector1.shape[1] - n_cond + 1))]])
-    vec_G1 = vector1@np.transpose(T)
-    vec_G2 = vector2@np.transpose(T)
+    vec_G1 = vector1 @ np.transpose(T)
+    vec_G2 = vector2 @ np.transpose(T)
 
     sim = _all_combinations(vec_G1, vec_G2, _riemannian_distance, sigma_k_hat)
     return sim
@@ -301,7 +305,7 @@ def _all_combinations(vectors1, vectors2, func, *args, **kwargs):
     return value
 
 
-def _cosine_cov_weighted_slow(vector1, vector2, sigma_k=None, nan_idx=None):
+def _cosine_cov_weighted_slow(vector1, vector2, frozen_inds=[], sigma_k=None, nan_idx=None):
     """computes the cosine similarities between two sets of vectors
     after whitening by their covariance.
 
@@ -327,6 +331,9 @@ def _cosine_cov_weighted_slow(vector1, vector2, sigma_k=None, nan_idx=None):
     else:
         n_cond = _get_n_from_reduced_vectors(vector1)
         v = _get_v(n_cond, sigma_k)
+    # Now adjust v to account for any frozen patterns.
+    v = _correct_covariance_for_frozen_patterns(v, n_cond, frozen_inds)
+
     # compute V^-1 vector1/2 for all vectors by solving Vx = vector1/2
     vector1_m = np.array([scipy.sparse.linalg.cg(v, vector1[i], atol=0)[0]
                           for i in range(vector1.shape[0])])
@@ -343,7 +350,37 @@ def _cosine_cov_weighted_slow(vector1, vector2, sigma_k=None, nan_idx=None):
     return cos
 
 
-def _cosine_cov_weighted(vector1, vector2, sigma_k=None, nan_idx=None):
+def _correct_covariance_for_frozen_patterns(v, n_cond, frozen_inds):
+    '''Rules:
+        1. If a shared pattern is frozen, set covariance to zero
+        2. If one pattern in a pair is frozen, set variance to half.
+        3. If both patterns frozen, set variance to zero.
+    '''
+    frozen_inds = set(frozen_inds)
+    if len(frozen_inds) == 0:
+        return v
+    for (i, (x1, y1)), (j, (x2, y2)) in it.product(enumerate(it.combinations(range(n_cond), 2)),
+                                                   enumerate(it.combinations(range(n_cond), 2))):
+        if len(np.unique([x1, y1, x2, y2])) == 4:  # if no shared patterns, skip
+            continue
+
+        if len(np.unique([x1, y1, x2, y2])) == 2:  # if the same pair:
+            num_frozen = np.sum([num in frozen_inds for num in [x1, y1]])
+            if num_frozen == 2:
+                v[i, j] = 0
+            elif num_frozen == 1:
+                v[i, j] /= 2
+
+        if len(np.unique([x1, y1, x2, y2])) == 3:  # if one shared pattern
+            # Check if the shared pattern is frozen, and if so, set the entry to zero.
+            rep_ind = mode([x1, y1, x2, y2])
+            if rep_ind in frozen_inds:
+                v[i, j] = 0
+
+    return v
+
+
+def _cosine_cov_weighted(vector1, vector2, frozen_inds=[], sigma_k=None, nan_idx=None):
     """computes the cosine angles between two sets of vectors
     weighted by the covariance
     If no covariance is given this is computed using the linear CKA,
@@ -365,7 +402,7 @@ def _cosine_cov_weighted(vector1, vector2, sigma_k=None, nan_idx=None):
     """
     if (sigma_k is not None) and (sigma_k.ndim >= 2):
         cos = _cosine_cov_weighted_slow(
-            vector1, vector2, sigma_k=sigma_k, nan_idx=nan_idx)
+            vector1, vector2, frozen_inds=frozen_inds, sigma_k=sigma_k, nan_idx=nan_idx)
     else:
         if nan_idx is None:
             nan_idx = np.ones(vector1[0].shape, bool)
@@ -428,8 +465,8 @@ def _cov_weighting(vector, nan_idx, sigma_k=None):
         diag = np.concatenate((np.ones((n_dist, 1)) / 2, np.ones((n_cond, 1))))
         # one line version much faster here!
         vector_w = vector_w - (
-            vector_w
-            @ sumI @ np.linalg.inv(sumI.T @ (diag * sumI)) @ (diag * sumI).T)
+                vector_w
+                @ sumI @ np.linalg.inv(sumI.T @ (diag * sumI)) @ (diag * sumI).T)
         if sigma_k is not None:
             if sigma_k.ndim == 1:
                 sigma_k_sqrt = np.sqrt(sigma_k)
@@ -492,12 +529,13 @@ def _riemannian_distance(vec_G1, vec_G2, sigma_k):
                 negative riemannian distance
     """
     n_cond = _get_n_from_length(len(vec_G1))
-    G1 = np.diag(vec_G1[0:(n_cond-1)])+squareform(vec_G1[(n_cond-1):len(vec_G1)])
-    G2 = np.diag(vec_G2[0:(n_cond-1)])+squareform(vec_G2[(n_cond-1):len(vec_G2)])
+    G1 = np.diag(vec_G1[0:(n_cond - 1)]) + squareform(vec_G1[(n_cond - 1):len(vec_G1)])
+    G2 = np.diag(vec_G2[0:(n_cond - 1)]) + squareform(vec_G2[(n_cond - 1):len(vec_G2)])
 
     def fun(theta):
         return np.sqrt((np.log(linalg.eigvalsh(
-            np.exp(theta[0]) * G1 + np.exp(theta[1]) * sigma_k, G2))**2).sum())
+            np.exp(theta[0]) * G1 + np.exp(theta[1]) * sigma_k, G2)) ** 2).sum())
+
     theta = minimize(fun, (0, 0), method='Nelder-Mead')
     neg_riem = -1 * theta.fun
     return neg_riem
@@ -542,8 +580,8 @@ def _tau_a(vector1, vector2):
                       (vector2[1:] != vector2[:-1]), True]
     cnt = np.diff(np.nonzero(obs)[0]).astype('int64', copy=False)
     ntie = (cnt * (cnt - 1) // 2).sum()  # joint ties
-    xtie, _, _ = _count_rank_tie(vector1)     # ties in x, stats
-    ytie, _, _ = _count_rank_tie(vector2)     # ties in y, stats
+    xtie, _, _ = _count_rank_tie(vector1)  # ties in x, stats
+    ytie, _, _ = _count_rank_tie(vector2)  # ties in y, stats
     tot = (size * (size - 1)) // 2
     # Note that tot = con + dis + (xtie - ntie) + (ytie - ntie) + ntie
     #               = con + dis + xtie + ytie - ntie
@@ -569,7 +607,7 @@ def _count_rank_tie(ranks):
     cnt = cnt[cnt > 1]
     return ((cnt * (cnt - 1) // 2).sum(),
             (cnt * (cnt - 1.) * (cnt - 2)).sum(),
-            (cnt * (cnt - 1.) * (2*cnt + 5)).sum())
+            (cnt * (cnt - 1.) * (2 * cnt + 5)).sum())
 
 
 def _get_v(n_cond, sigma_k):