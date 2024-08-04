dabe64d044d96445b23192059c3933c1d136cd36
Jasper van den Bosch <japsai@gmail.com>
2024-08-04 12:37:35 +0100

    assistant writes summary

--- devops/assistant.py
+++ devops/assistant.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
-"""Helper functions for our continuous integration suite on Github Actions 
-"""
+"""Helper functions for our continuous integration suite on Github Actions"""
+
 import os
+
+THRESHOLD = 0  # How many errors are allowed
+
 fpath = 'ruff.log'
 with open(fpath) as fhandle:
-    print(fhandle.readlines()[0])
+    lines = fhandle.readlines()
+lines.reverse()
+for line in lines:
+    if line.startswith('Found '):
+        n = int(line.split()[1])
 
 
-if "GITHUB_STEP_SUMMARY" in os.environ :
-    with open(os.environ["GITHUB_STEP_SUMMARY"], "a") as fhandle :
-        fhandle.write('hello from python')
+summary_issues = """
+### Ruff: {} issues found :construction:
+
+See individual errors below under *Annotations*, or 
+see them inline at the *Files Changed* tab of the Pull Request.
+
+You can also run install ruff and then run `ruff check` on your machine.
+"""
+
+summary_ok = """
+### Ruff: No issues found :yellow_heart:
+"""
+
+summary = summary_issues.format(n) if n > THRESHOLD else summary_ok
+
+if 'GITHUB_STEP_SUMMARY' in os.environ:
+    with open(os.environ['GITHUB_STEP_SUMMARY'], 'a') as fhandle:
+        fhandle.write(summary)
