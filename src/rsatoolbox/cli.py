c2f38365999c76b126dd5ef8361c898091d57fb6
Jasper van den Bosch <japsai@gmail.com>
2024-11-07 18:13:30 +0000

    add version command

--- src/rsatoolbox/cli.py
+++ src/rsatoolbox/cli.py
@@ -1,10 +1,21 @@
 """Command Line Interface
 """
 from argparse import ArgumentParser
+from importlib.metadata import version
 
 parser = ArgumentParser(
     prog='rsatoolbox',
     description='Representational Similarity Analysis'
+)
+parser.add_argument(
+    'file',
+    default='.',
+    help='Data directory or file'
+)  
+parser.add_argument(
+    '--version',
+    action='version',
+    version=f'rsatoolbox {version("rsatoolbox")}'
 )
 
 
