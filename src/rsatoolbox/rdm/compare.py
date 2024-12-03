Fixed the calculation of the RDM covariance matrix for the frozen patterns.

src/rsatoolbox/rdm/compare.py
@@ -138,8 +138,8 @@ def compare_cosine_cov_weighted(rdm1, rdm2, sigma_k=None):
     vector1, vector2, nan_idx = _parse_input_rdms(rdm1, rdm2)
     stim_nums = np.array(rdm1.pattern_descriptors['stimulus'])
     frozen_inds = list(np.where(stim_nums == -1)[0]) + list(np.where(stim_nums == -2)[0])
-    # sigma_k[frozen_inds, :] = 0
-    # sigma_k[:, frozen_inds] = 0
+    sigma_k[frozen_inds, :] = 0
+    sigma_k[:, frozen_inds] = 0
     sim = _cosine_cov_weighted(vector1, vector2, frozen_inds, sigma_k, nan_idx)
     return sim
 
@@ -340,7 +340,7 @@ def _cosine_cov_weighted_slow(vector1, vector2, frozen_inds=[], sigma_k=None, na
         n_cond = _get_n_from_reduced_vectors(vector1)
         v = _get_v(n_cond, sigma_k)
     # Now adjust v to account for any frozen patterns.
-    v = _correct_covariance_for_frozen_patterns(v, n_cond, frozen_inds)
+    # v = _correct_covariance_for_frozen_patterns(v, n_cond, frozen_inds)
     # Omit any all-zero rows and columns, keeping as a sparse matrix.
     nonzero_rows = np.where(v.sum(axis=1) != 0)[0]
     v = v[nonzero_rows][:, nonzero_rows]