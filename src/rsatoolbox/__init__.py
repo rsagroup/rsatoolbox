695e723ee216d581b836c507f275ac7b03b7dd70
Heiko Schütt <heiko.schutt@uni.lu>
2026-07-13 16:17:37 +0200

    get the right version ....

--- src/rsatoolbox/__init__.py
+++ src/rsatoolbox/__init__.py
@@ -3,12 +3,8 @@
 """ Top level package: Only imports and organisation
 """
 
-try:
-    from importlib.metadata import version
-
-    __version__ = version("mne")
-except Exception:
-    __version__ = "0.0.0"
+from importlib.metadata import version
+__version__ = version("rsatoolbox")
 
 from . import cengine
 from . import data
