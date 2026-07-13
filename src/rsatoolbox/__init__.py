d072b301ee7bdf56ff10a22fa928899226257f07
Heiko Schütt <heiko.schutt@uni.lu>
2026-07-13 10:48:50 +0200

    version handling inspired by MNEpython

--- src/rsatoolbox/__init__.py
+++ src/rsatoolbox/__init__.py
@@ -3,6 +3,13 @@
 """ Top level package: Only imports and organisation
 """
 
+try:
+    from importlib.metadata import version
+
+    __version__ = version("mne")
+except Exception:
+    __version__ = "0.0.0"
+
 from . import cengine
 from . import data
 from . import inference
